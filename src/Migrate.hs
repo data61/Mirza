f486065d81f7bb0e031c1ba3570b4325d6e65b9e
Matthew Perry <Matt.Perry@data61.csiro.au>
2018-01-09 12:55:32 +1100

    tweaks

--- src/Migrate.hs
+++ src/Migrate.hs
@@ -2,11 +2,23 @@
 module Migrate where
 
 import StorageBeam -- the schemas
-import Database.Beam (withDatabaseDebug)
-import Database.Beam.Postgres (Connection)
-import Database.Beam.Migrate.Types (Migration, executeMigration)
+import Database.Beam
+import Database.Beam.Postgres (Connection, Postgres, PgCommandSyntax)
+import Database.Beam.Migrate.Types ( Migration
+                                   , executeMigration 
+                                   , CheckedDatabaseSettings
+                                   , MigrationSteps
+                                   , uncheckDatabase
+                                   , evaluateDatabase
+                                   , migrationStep )
 import Database.Beam.Backend (runNoReturn)
 
+import Data.Text (Text)
+import Database.Beam.Postgres.Migrate
+import Database.Beam.Migrate.SQL.Tables
+import Database.Beam.Migrate.SQL.Types
 
 
 
+
+migrateV1 :: () -> Migration PgCommandSyntax (CheckedDatabaseSettings Postgres )
