41d66ad43b406690583e17babc1e765080089a66
Sajid Ibne Anower <sajid.anower@data61.csiro.au>
2018-01-09 11:32:07 +1100

    Import libraries
    Former-commit-id: 2f3d7e9779e03453366827c2646de5aa8eead41f

--- src/Migrate.hs
+++ src/Migrate.hs
@@ -1,8 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Migrate where
 
-import StorageBeam
+import StorageBeam -- the schemas
+import Database.Beam (withDatabaseDebug)
+import Database.Beam.Postgres (Connection)
+import Database.Beam.Migrate.Types (Migration, executeMigration)
+import Database.Beam.Backend (runNoReturn)
 
-import Database.Beam
+
 
 
