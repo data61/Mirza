fc62d772b6ff9ab665f38f4bd9871191286a24c8
Matthew Perry <Matt.Perry@data61.csiro.au>
2018-01-09 13:00:46 +1100

    fix merge conflict

--- src/Migrate.hs
+++ src/Migrate.hs
@@ -2,7 +2,6 @@
 module Migrate where
 
 import StorageBeam -- the schemas
-<<<<<<< HEAD
 import Database.Beam
 import Database.Beam.Postgres (Connection, Postgres, PgCommandSyntax)
 import Database.Beam.Migrate.Types ( Migration
@@ -12,11 +11,6 @@ import Database.Beam.Migrate.Types ( Migration
                                    , uncheckDatabase
                                    , evaluateDatabase
                                    , migrationStep )
-=======
-import Database.Beam (withDatabaseDebug)
-import Database.Beam.Postgres (Connection)
-import Database.Beam.Migrate.Types
->>>>>>> 658660d01c1eb000390d6faed0697c54da4893df
 import Database.Beam.Backend (runNoReturn)
 
 import Data.Text (Text)
