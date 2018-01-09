c67f667370a0772bb6c2e3cb64ebb5b5ad12cbe8
Sajid Ibne Anower <sajid.anower@data61.csiro.au>
2018-01-09 12:52:13 +1100

    Fix import
    Former-commit-id: 49417e6e4878e8f9bee10fedae1bab43fea5a701

--- src/Migrate.hs
+++ src/Migrate.hs
@@ -4,7 +4,7 @@ module Migrate where
 import StorageBeam -- the schemas
 import Database.Beam (withDatabaseDebug)
 import Database.Beam.Postgres (Connection)
-import Database.Beam.Migrate.Types (Migration, executeMigration)
+import Database.Beam.Migrate.Types
 import Database.Beam.Backend (runNoReturn)
 
 
