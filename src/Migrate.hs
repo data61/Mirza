39d700da555b71f507421c8d9733f17e47cb958b
Sajid Ibne Anower <sajid.anower@data61.csiro.au>
2018-01-11 16:41:16 +1100

    Tweak

--- src/Migrate.hs
+++ src/Migrate.hs
@@ -13,10 +13,6 @@ import Database.PostgreSQL.Simple(SqlError ,connectPostgreSQL)
 import Database.Beam.Postgres (Connection)
 import Data.ByteString.Char8 (ByteString, pack)
 
--- import qualified Persistence.PaddockPersist as PP
--- import Persistence.Schema.Migration (createSchema)
--- import Persistence.Types (PaddockUpdate(..), PaddockInsert(..))
-
 connectionStr :: ByteString
 connectionStr = pack "dbname=testsupplychainserver"
 
@@ -25,22 +21,14 @@ createSchema conn = do
   withDatabaseDebug putStrLn conn $ executeMigration runNoReturn migrationStorage
   return ()
 
-
 tryCreateSchema :: Connection -> IO ()
 tryCreateSchema conn = E.catch (createSchema conn) handleErr
   where
     handleErr :: SqlError -> IO ()
-    handleErr e = putStrLn "Schema up to date"
-
+    handleErr e = print e
 
 migrate :: IO ()
 migrate = do
   conn <- connectPostgreSQL connectionStr
   tryCreateSchema conn
---   PP.insertPaddock conn (PaddockInsert (Just "Thanh's Paddock 2") (-33.8046874) 151.2708806 "Barley" "Wheat" "thanh")
---   PP.updatePaddock conn "thanh" (PaddockUpdate (PP.toPaddockId 1) (Just "Thanh's Paddock 1") Nothing Nothing)
---   removedPP <- PP.removePaddockUser conn "thanh" (PP.toPaddockId 8)
---   paddocks <- PP.selectPaddocks conn "thanh"
   print $ "Successfully created table. ConnectionStr was " ++ show connectionStr
-
-
