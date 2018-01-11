3e8790dc8199d7bd318e416eaa1bcd3f9dc8c2af
Matthew Perry <Matt.Perry@data61.csiro.au>
2018-01-11 17:48:11 +1100

    add basic query beam

--- src/Migrate.hs
+++ src/Migrate.hs
@@ -2,23 +2,9 @@
 module Migrate where
 
 import StorageBeam -- the schemas
-import Database.Beam
-import Database.Beam.Postgres (Connection, Postgres, PgCommandSyntax)
-import Database.Beam.Migrate.Types ( Migration
-                                   , executeMigration 
-                                   , CheckedDatabaseSettings
-                                   , MigrationSteps
-                                   , uncheckDatabase
-                                   , evaluateDatabase
-                                   , migrationStep )
+import Database.Beam (withDatabaseDebug)
+import Database.Beam.Postgres (Connection)
+import Database.Beam.Migrate.Types
 import Database.Beam.Backend (runNoReturn)
 
-import Data.Text (Text)
-import Database.Beam.Postgres.Migrate
-import Database.Beam.Migrate.SQL.Tables
-import Database.Beam.Migrate.SQL.Types
 
-
-
-
-migrateV1 :: () -> Migration PgCommandSyntax (CheckedDatabaseSettings Postgres )
