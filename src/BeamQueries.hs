419faf695bed552e9819609eb495118903169472
Matthew Perry <Matt.Perry@data61.csiro.au>
2018-01-12 19:44:33 +1100

    add partial completion of eventSign
    Former-commit-id: 6cad8a7148cd788cb81a130a59074cb48eb0c101

--- src/BeamQueries.hs
+++ src/BeamQueries.hs
@@ -49,9 +49,9 @@ type DBConn = Connection
 
 insertUser :: DBConn -> DBFunc -> EncryptedPass -> M.NewUser -> IO M.UserID
 insertUser conn dbFunc pass (M.NewUser phone email first last biz password) = do
-  rowID <-dbFunc conn $ last $ runInsertReturningList $
+  rowID <- dbFunc conn $ ((last $ runInsertReturningList $
         (supplyChainDb ^. _users) $
-          insertValues [(User (Auto Nothing) biz first last phone hash email)]
+          insertValues [(User (Auto Nothing) biz first last phone hash email)]) ^. userId)
    return (fromIntegral rowID :: M.UserID)
 
 newUser :: DBConn -> DBFunc -> M.NewUser -> IO M.UserID
@@ -98,9 +98,9 @@ authCheck conn dbFunc email password = do
 addPublicKey :: DBConn -> DBFunc -> M.User -> M.RSAPublicKey-> IO M.KeyID
 addPublicKey conn dbFunc (M.User uid _ _)  (M.RSAPublicKey n e) = do
   timestamp <- getCurrentTime
-  rowID <- dbFunc conn $ last $ runInsertReturningList $
+  rowID <- dbFunc conn $ ((last $ runInsertReturningList $
            (supplyChainDb ^. _keys) $
-             insertValues [(Key (Auto Nothing) uid n e timestamp 0)] -- TODO = check if 0 is correct here... NOT SURE
+             insertValues [(Key (Auto Nothing) uid n e timestamp 0)]) ^. _keyId) -- TODO = check if 0 is correct here... NOT SURE
   return (fromIntegral rowID :: M.KeyID)
 
 -- TODO = fix. 1 problem is nothing is done with filter value or asset type in objectRowID grabbing data insert
@@ -108,16 +108,16 @@ addPublicKey conn dbFunc (M.User uid _ _)  (M.RSAPublicKey n e) = do
 -- epc is a labelEPC
 eventCreateObject :: DBConn -> DBFunc -> M.User -> M.NewObject -> IO Event
 eventCreateObject conn dbFunc (M.User uid _ _ ) (M.NewObject epc epcisTime timezone location) = do
-  objectRowID <- dbFunc conn $ last $ runInsertReturningList $ (supplyChainDb ^. _labels) $
+  objectRowID <- dbFunc conn $ ((last $ runInsertReturningList $ (supplyChainDb ^. _labels) $
                    (case epc of
                      IL il   -> (case il of
                                  GIAI cp sn        -> insertValues [(Label (Auto Nothing) cp Nothing sn Nothing "GIAI" Nothing)]
                                  SSCC cp sn        -> insertValues [(Label (Auto Nothing) cp Nothing sn Nothing "SSCC" Nothing)]
                                  SGTIN cp fv ir sn -> insertValues [(Label (Auto Nothing) cp ir sn Nothing "SGTIN" Nothing)]
-                                 GRAI cp at sn     -> insertValues [(Label (Auto Nothing) cp Nothing sn Nothing "GIAI" Nothing)])
+                                 GRAI cp at sn     -> insertValues [(Label (Auto Nothing) cp Nothing sn Nothing "GRAI" Nothing)])
                      CL cl q -> (case cl of
                                  LGTIN cp ir lot   -> insertValues [(Label (Auto Nothing) cp ir Nothing Nothing "LGTIN" lot)]
-                                 CSGTIN cp fv ir   -> insertValues [(Label (Auto Nothing) cp ir Nothing Nothing "CSGTIN" Nothing)]))
+                                 CSGTIN cp fv ir   -> insertValues [(Label (Auto Nothing) cp ir Nothing Nothing "CSGTIN" Nothing)]))) ^. _labelId)
 
   currentTime <- getCurrentTime
   uuid <- nextRandom
@@ -142,3 +142,68 @@ eventCreateObject conn dbFunc (M.User uid _ _ ) (M.NewObject epc epcisTime timez
 
   return event
 
+eventSign :: (MonadError M.SigError m, MonadIO m) => DBConn -> DBFunc -> M.User -> M.SignedEvent -> m ()
+eventSign conn dbFunc (M.User uid _ _ ) (M.SignedEvent eventID keyID (M.Signature signature)) = do
+  timestamp <- liftIO getCurrentTime
+  rFull <- dbFunc conn $ runSelectReturningList $ select $ do
+    allKeys <- all_ (supplyChainDb ^. _keys)
+    guard_ (allKeys ^. _keyId ==. keyID)
+    pure allKeys
+    
+  r <- zip ((\e -> e ^. _rsa_n) <$> rFull) ((\e -> e ^. _rsa_e) <$> rFull)
+
+  pubkey <- if length r == 0
+    then throwError M.SE_InvalidKeyID
+    else
+      return $ uncurry M.RSAPublicKey $ head r
+
+  r <- dbFunc conn $ ((\e -> e ^. _jsonEvent) <$> (runSelectReturningList $ select $ do
+       allEvents <- all_ (supplyChainDb ^. _events)
+       guard_ (allEvents ^. _eventId ==. eventID)
+       pure allEvents))
+
+  blob <- case r of
+            [Only (x::String)] -> return $ pack x
+            _      -> throwError M.SE_InvalidEventID
+
+  checkSignature pubkey blob (M.Signature signature)
+
+  -- TODO = note that there is no hashes table now, so insert into hashes excluded
+  -- TODO = userevents is combination of biztransactiontable and _eventCreatedBy field in event table, explore this
+
+  checkReadyForBlockchain conn eventID
+  package <- createBlockchainPackage conn eventID
+  liftIO $ sendToBlockchain package
+
+-- TODO - convert these below functions, and others in original file Storage.hs
+
+createBlockchainPackage ::  (MonadError M.SigError m, MonadIO m) => DBConn -> EventID -> m C.BlockchainPackage
+createBlockchainPackage conn eventID = do
+  -- XXX do we want to explicitly check that the hash is signed before assuming the first one is not?
+  r <- liftIO $ query_ conn "SELECT hash, signedByUserID FROM Hashes WHERE eventID=? ORDER BY isSigned ASC;" $ Only eventID
+  if length r > 2
+    then
+      let (plainHash, userID) = head r
+          signatures = NonEmpty.fromList (map (\(s, u) -> ((M.Signature s), u)) (tail r))
+      in
+        return $ C.BlockchainPackage (M.EventHash plainHash) signatures
+    else throwError M.SE_BlockchainSendFailed
+
+--TODO - Implement me
+-- sendToBlockchain ::  (MonadError M.SigError m, MonadIO m) =>  C.BlockchainPackage -> m ()
+sendToBlockchain :: Monad m => C.BlockchainPackage -> m ()
+sendToBlockchain package = return () -- if it fails, raise SE_SEND_TO_BLOCKCHAIN_FAILED error.
+
+checkSignature :: (MonadError M.SigError m, MonadIO m) => M.RSAPublicKey -> ByteString.ByteString -> M.Signature -> m ()
+checkSignature pubkey blob signature =
+  unless (C.verifySignature pubkey blob signature) $
+    throwError M.SE_InvalidSignature
+
+-- ready to send to blockchain when all the parties have signed
+checkReadyForBlockchain :: (MonadError M.SigError m, MonadIO m) => DBConn -> EventID -> m ()
+--checkReadyForBlockchain conn eventID = undefined
+checkReadyForBlockchain conn eventID = do
+  r <- liftIO $ query_ conn "SELECT COUNT(id) FROM UserEvents WHERE eventID=? AND hasSigned=FALSE;" $ Only eventID
+  case r of
+    [Only (0::Integer)] -> pure ()
+    _ -> throwError M.SE_NeedMoreSignatures
