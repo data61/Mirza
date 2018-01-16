d9b82c7f838ccf1609dc9c713d2a09cf11a20d2e
Sajid Ibne Anower <sajid.anower@data61.csiro.au>
2018-01-16 12:53:14 +1100

    Add script to rebuild stuff

---
+++ restart.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+# This script is for people who are lazy like me
+# Run this to undo all database work, compile the code
+# and rerun the server
+# Ideally to be run during testing phase
+echo Recreating the database
+./manage_db.sh testsupplychainserver
+
+GIVEN_OPT=$1
+OPTION=${GIVEN_OPT:="some_random_text"}
+
+echo Building the modules
+if test $OPTION = '--from-scratch'
+then
+    stack clean
+fi
+stack build
+stack exec supplyChainServer-exe -- run migrate
