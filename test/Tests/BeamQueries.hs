7212c2d2808b71c96f7829b3ac1d5bed475f57a5
Matthew Perry <Matt.Perry@data61.csiro.au>
2018-01-22 16:24:22 +1100

    add missing files
    Former-commit-id: a4d915b75b2fa2be99e57be450d11d9a3e4277eb

---
+++ test/Tests/BeamQueries.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE DataKinds #-}
+
+module Tests.BeamQueries where
+
+import           Test.Hspec
+import           Database.PostgreSQL.Simple
+
+import           BeamQueries
+
+testNewUser :: SpecWith Connection
+testNewUser = do
+  describe "blah" $ do
+    it "blah" $ \c -> do
+      1 `shouldBe` 1
