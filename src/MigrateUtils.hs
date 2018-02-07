114db80bafac3bca3448d806041b618667f11596
Sajid Ibne Anower <sajid.anower@data61.csiro.au>
2018-02-07 15:16:03 +1100

    Add new modules for refactoring code into later
    Former-commit-id: 42db7d7b0a0725a2c521ed876b903e66f47ddfec

---
+++ src/MigrateUtils.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE OverloadedStrings     #-}
+
+module MigrateUtils
+-- (
+--   eventType
+-- )
+where
+
+-- import           Database.Beam.Postgres.Migrate (PgDataTypeSyntax, pgTextType)
+-- import qualified Data.GS1.Event as Ev
+
+-- eventType :: DataType PgDataTypeSyntax Ev.EventType
+-- eventType = DataType pgTextType
+
