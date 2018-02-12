5cedf9ac12c00264d3e94029ebb12c9c6136895f
Sajid Ibne Anower <sajid.anower@data61.csiro.au>
2018-02-12 12:29:27 +1100

    Add some migration types

--- src/MigrateUtils.hs
+++ src/MigrateUtils.hs
@@ -3,15 +3,19 @@
 -- Database.Beam.Postgres.Syntax.DataType definitions
 -- At the moment, if Database.Beam.Postgres.Syntax is a hidden module
 -- So it is not possible to implement the types yet
-module MigrateUtils
--- (
---   eventType
--- )
-where
+module MigrateUtils where
 
--- import           Database.Beam.Postgres.Migrate (PgDataTypeSyntax, pgTextType)
--- import qualified Data.GS1.Event as Ev
+import           Database.Beam.Postgres.Syntax (PgDataTypeSyntax, pgTextType)
+import qualified Data.GS1.Event as Ev
+import qualified Data.GS1.EPC as EPC
+import           Database.Beam.Migrate (DataType(..))
 
--- eventType :: DataType PgDataTypeSyntax Ev.EventType
--- eventType = DataType pgTextType
+-- | Shorthand for using postgres text type
+textType :: DataType PgDataTypeSyntax a
+textType = DataType pgTextType
 
+eventType :: DataType PgDataTypeSyntax Ev.EventType
+eventType = textType
+
+actionType :: DataType PgDataTypeSyntax EPC.Action
+actionType = textType
