d087f3a32ed67ca0d6ab36d5d2351fcb24cf0c38
Sajid Ibne Anower <sajid.anower@data61.csiro.au>
2018-02-16 11:43:00 +1100

    Add error handling for all used types
    Former-commit-id: 2119d96e616146a9735f5d83b416b6303455ed47

--- src/ErrorUtils.hs
+++ src/ErrorUtils.hs
@@ -15,8 +15,35 @@ import           Data.ByteString (ByteString(..))
 appErrToHttpErr :: ServiceError -> Handler a
 appErrToHttpErr (EmailExists _ email) =
   throwError $ err400 {
-    errBody = LBSC8.fromChunks $ ["User email ", encodeUtf8 email, " exists"]}
+    errBody = LBSC8.fromChunks $ ["User email ", encodeUtf8 email, " exists."]
+  }
+appErrToHttpErr (UnexpectedDBResponse _) =
+  throwError $ err500 {
+    errBody = "We received an unexpected response from our database. This error has been logged and someone is looking into it."
+  }
+appErrToHttpErr (UserNotFound email) =
+  throwError $ err404 {
+    errBody = LBSC8.fromChunks $ ["User with email ", encodeUtf8 email, " could not be found."]
+  }
+appErrToHttpErr (AuthFailed email) =
+  throwError $ err404 {
+    errBody = LBSC8.fromChunks $ ["Authentication failed for email ", encodeUtf8 email, "."]
+  }
+appErrToHttpErr (EmailNotFound email) =
+  throwError $ err404 {
+    errBody = LBSC8.fromChunks $ ["Email ", encodeUtf8 email, " could not be found."]
+  }
+appErrToHttpErr (InsertionFail _ email) =
+  throwError $ err500 {
+    errBody = LBSC8.fromChunks $ ["Email ", encodeUtf8 email, " could not be inserted."]
+  }
+appErrToHttpErr (InvalidKeyID _) =
+  throwError $ err400 {
+    errBody = LBSC8.fromChunks $ ["Invalid Key ID entered."]
+  }
+
 appErrToHttpErr _ = throwError err500 {errBody = "The server did not understand this request."}
+
 
 toServerError :: Show a => (a -> Maybe ErrorCode) -> a -> ServerError
 toServerError f e = ServerError (f e) (toText e)
