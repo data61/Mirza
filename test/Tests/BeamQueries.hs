781d0378513f069c132a7de98376102d95f5a138
Sajid Ibne Anower <sajid.anower@data61.csiro.au>
2018-02-28 16:04:12 +1100

    Add stub for new test

--- test/Tests/BeamQueries.hs
+++ test/Tests/BeamQueries.hs
@@ -29,11 +29,33 @@ import qualified Model as M
 -- NOTE in this file, where fromJust is used in the tests, it is because we expect a Just... this is part of the test
 -- NOTE tables dropped after every running of test in an "it"
 
-user1 = (M.NewUser "000" "fake@gmail.com" "Bob" "Smith" "blah Ltd" "password")
+dummyNewUser :: M.NewUser
+dummyNewUser = M.NewUser "000" "fake@gmail.com" "Bob" "Smith" "blah Ltd" "password"
+
+sampleObjectFile :: FilePath
+sampleObjectFile = "../GS1Combinators/test/test-xml/ObjectEvent.xml"
+
+dummyUser :: M.User
+dummyUser = M.User nil "Sajid" "Anower"
+
+runEventCreateObject :: FilePath -> AppM ()
+runEventCreateObject xmlFile = do
+  doc <- liftIO $ Text.XML.readFile def xmlFile
+  let mainCursor = fromDocument doc
+  -- scope for optimization: only call parseEventByType on existent EventTypes
+      allParsedEvents =
+        filter (not . null) $ concat $
+        parseEventByType mainCursor <$> allEventTypes
+      (Right objEvent) = head allParsedEvents
+      newObj = M.mkObjectEvent objEvent
+  eventId <- eventCreateObject dummyUser newObj
+  liftIO $ print eventId
+  -- liftIO $ print objEvent
+  -- mapM_ (TL.putStrLn . TLE.decodeUtf8 . encodePretty) (rights allParsedEvents)
 
 -- for grabbing the encrypted password from user 1
 hashIO :: MonadIO m => m ByteString
-hashIO = getEncryptedPass <$> (liftIO $ encryptPassIO' (Pass $ encodeUtf8 $ M.password user1))
+hashIO = getEncryptedPass <$> (liftIO $ encryptPassIO' (Pass $ encodeUtf8 $ M.password dummyNewUser))
 
 selectUser :: M.UserID -> AppM (Maybe SB.User)
 selectUser uid = do
@@ -46,12 +68,13 @@ selectUser uid = do
     Right [user] -> return $ Just user
     _            -> return Nothing
 
+
 testQueries :: SpecWith (Connection, Env)
 testQueries = do
   describe "newUser tests" $ do
     it "newUser test 1" $ \(conn, env) -> do
         hash <- hashIO
-        uid <- fromRight' <$> (runAppM env $ newUser user1)
+        uid <- fromRight' <$> (runAppM env $ newUser dummyNewUser)
         user <- fromRight' <$> (runAppM env $ selectUser uid)
 
         --print $ show hash
@@ -59,28 +82,29 @@ testQueries = do
 
         (fromJust user) `shouldSatisfy`
             (\u ->
-              (SB.phone_number u) == (M.phoneNumber user1) &&
-              (SB.email_address u) == (M.emailAddress user1) &&
-              (SB.first_name u) == (M.firstName user1) &&
-              (SB.last_name u) == (M.lastName user1) &&
-              (SB.user_biz_id u) == (SB.BizId (M.company user1)) &&
+              (SB.phone_number u) == (M.phoneNumber dummyNewUser) &&
+              (SB.email_address u) == (M.emailAddress dummyNewUser) &&
+              (SB.first_name u) == (M.firstName dummyNewUser) &&
+              (SB.last_name u) == (M.lastName dummyNewUser) &&
+              (SB.user_biz_id u) == (SB.BizId (M.company dummyNewUser)) &&
               -- note database bytestring includes the salt, this checks password
-              (verifyPass' (Pass $ encodeUtf8 $ M.password user1) (EncryptedPass $ SB.password_hash u)) &&
+              (verifyPass' (Pass $ encodeUtf8 $ M.password dummyNewUser) (EncryptedPass $ SB.password_hash u)) &&
               (SB.user_id u) == uid)
 
   describe "authCheck tests" $ do
     it "authCheck test 1" $ \(conn, env) -> do
       --hash <- hashIO
-      uid <- fromRight' <$> (runAppM env $ newUser user1)
-      user <- fromRight' <$> (runAppM env $ authCheck (M.emailAddress user1) (encodeUtf8 $ M.password user1)) --hash)
+      uid <- fromRight' <$> (runAppM env $ newUser dummyNewUser)
+      user <- fromRight' <$> (runAppM env $ authCheck (M.emailAddress dummyNewUser) (encodeUtf8 $ M.password dummyNewUser)) --hash)
       (fromJust user) `shouldSatisfy`
         (\u -> (M.userId u) == uid &&
-               (M.userFirstName u) == (M.firstName user1) &&
-               (M.userLastName u) == (M.lastName user1))
+               (M.userFirstName u) == (M.firstName dummyNewUser) &&
+               (M.userLastName u) == (M.lastName dummyNewUser))
 
-  describe "Object Event" $ do
-    it "eventCreateObject" $ \(conn, env) -> do
-      1 `shouldBe` 1
+  -- describe "Object Event" $ do
+  --   it "eventCreateObject" $ \(conn, env) -> do
+  --     runEventCreateObject sampleObjectFile
+  --     1 `shouldBe` 1
 
 
 
