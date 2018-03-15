4c47fca5425644e103552c1bfa9097d0b5255b7d
Sajid Ibne Anower <sajid.anower@data61.csiro.au>
2018-03-15 15:46:24 +1100

    Style tweaks

--- src/API.hs
+++ src/API.hs
@@ -42,10 +42,11 @@ type PrivateAPI =
   :<|> "event" :> "transformObject" :> ReqBody '[JSON] TransformationEvent :> Post '[JSON] PrimaryKeyType
   :<|> "key" :> "add" :>  ReqBody '[JSON] RSAPublicKey :> Post '[JSON] KeyID
 
-type PublicAPI =   "newUser" :> ReqBody '[JSON] NewUser :> Post '[JSON] UserID
-            :<|> "key" :> "get" :> Capture "keyID" KeyID :> Get '[JSON] RSAPublicKey
-            :<|> "key" :> "getInfo" :> Capture "keyID" KeyID :> Get '[JSON] KeyInfo
-            :<|> "business" :> "list" :> Get '[JSON] [Business]
+type PublicAPI =
+       "newUser" :> ReqBody '[JSON] NewUser :> Post '[JSON] UserID
+  :<|> "key" :> "get" :> Capture "keyID" KeyID :> Get '[JSON] RSAPublicKey
+  :<|> "key" :> "getInfo" :> Capture "keyID" KeyID :> Get '[JSON] KeyInfo
+  :<|> "business" :> "list" :> Get '[JSON] [Business]
 
 type SwaggerAPI = SwaggerSchemaUI "swagger-ui" "swagger.json"
 
