73578088920b5fb1a9de481ef75d057c9045d857
Alex Mason <Alex.Mason@data61.csiro.au>
2018-04-05 13:59:46 +1000

    Revert "Make API.hs more consistent"
    This reverts commit 171459af41343d21457763db0087281bf33a68ed.

--- src/API.hs
+++ src/API.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE CPP                   #-}
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
@@ -6,6 +5,7 @@
 {-# LANGUAGE ScopedTypeVariables   #-}
 {-# LANGUAGE TypeFamilies          #-}
 {-# LANGUAGE TypeOperators         #-}
+{-# LANGUAGE CPP                   #-}
 {-# LANGUAGE UndecidableInstances  #-}
 {-# OPTIONS_GHC -fno-warn-orphans  #-}
 {-# LANGUAGE DataKinds             #-}
@@ -16,30 +16,30 @@ module API where
 -- import           Prelude.Compat
 import           Servant
 -- import           Servant.Server.Experimental.Auth()
-import qualified Data.GS1.Event     as Ev
-import           Data.GS1.EventID
-import           Data.Swagger
-import           Model              as M
 import           Servant.Swagger.UI
-import           StorageBeam        (PrimaryKeyType)
+import           Data.Swagger
+import qualified Data.GS1.Event as Ev
+import           Data.GS1.EventID
+import           Model as M
+import           StorageBeam (PrimaryKeyType)
 
 type PrivateAPI =
-       "epc"      :> Capture "urn" String      :> "info"   :> Get '[JSON] EPCState
-  :<|> "epc"      :> Capture "urn" String      :> "events" :> Get '[JSON] [Ev.Event]
-  :<|> "event"    :> Capture "eventID" EventID :> "info"   :> Get '[JSON] Ev.Event
-  :<|> "contacts" :> Get '[JSON] [User]
-  :<|> "contacts" :> "add"               :> Capture "userID" PrimaryKeyType :> Get '[JSON] Bool
-  :<|> "contacts" :> "remove"            :> Capture "userID" PrimaryKeyType :> Get '[JSON] Bool
-  :<|> "contacts" :> "search"            :> Capture "term" String :> Get '[JSON] [User]
-  :<|> "event"    :> "list"              :> Capture "userID" PrimaryKeyType :> Get '[JSON] [Ev.Event]
-  :<|> "event"    :> "listUsers"         :> Capture "eventID" EventID :> Get '[JSON] [(User, Bool)]
-  :<|> "event"    :> "sign"              :> ReqBody '[JSON] SignedEvent :> Post '[JSON] PrimaryKeyType
-  :<|> "event"    :> "getHash"           :> ReqBody '[JSON] EventID :> Post '[JSON] HashedEvent
-  :<|> "event"    :> "objectEvent"       :> ReqBody '[JSON] ObjectEvent :> Post '[JSON] Ev.Event
-  :<|> "event"    :> "aggregateObjects"  :> ReqBody '[JSON] AggregationEvent :> Post '[JSON] Ev.Event
-  :<|> "event"    :> "start-transaction" :> ReqBody '[JSON] TransactionEvent :> Post '[JSON] Ev.Event
-  :<|> "event"    :> "transformObject"   :> ReqBody '[JSON] TransformationEvent :> Post '[JSON] Ev.Event
-  :<|> "key"      :> "add"               :> ReqBody '[JSON] RSAPublicKey :> Post '[JSON] KeyID
+       "epc" :>  Capture "urn" String:> "info" :> Get '[JSON] EPCState
+  :<|> "epc" :> Capture "urn" String :> "events" :> Get '[JSON] [Ev.Event]
+  :<|> "event" :> Capture "eventID" EventID:> "info" :> Get '[JSON] Ev.Event
+  :<|> "contacts" :>  Get '[JSON] [User]
+  :<|> "contacts" :> "add" :> Capture "userID" PrimaryKeyType :> Get '[JSON] Bool
+  :<|> "contacts" :> "remove" :> Capture "userID" PrimaryKeyType :> Get '[JSON] Bool
+  :<|> "contacts" :> "search" :> Capture "term" String :> Get '[JSON] [User]
+  :<|> "event" :> "list" :> Capture "userID" PrimaryKeyType :> Get '[JSON] [Ev.Event]
+  :<|> "event" :> "listUsers" :> Capture "eventID" EventID :> Get '[JSON] [(User, Bool)]
+  :<|> "event" :> "sign" :> ReqBody '[JSON] SignedEvent :> Post '[JSON] PrimaryKeyType
+  :<|> "event" :> "getHash" :> ReqBody '[JSON] EventID :> Post '[JSON] HashedEvent
+  :<|> "event" :> "objectEvent" :> ReqBody '[JSON] ObjectEvent :> Post '[JSON] Ev.Event
+  :<|> "event" :> "aggregateObjects" :> ReqBody '[JSON] AggregationEvent :> Post '[JSON] Ev.Event
+  :<|> "event" :> "start-transaction" :> ReqBody '[JSON] TransactionEvent :> Post '[JSON] Ev.Event
+  :<|> "event" :> "transformObject" :> ReqBody '[JSON] TransformationEvent :> Post '[JSON] Ev.Event
+  :<|> "key" :> "add" :>  ReqBody '[JSON] RSAPublicKey :> Post '[JSON] KeyID
 
 type PublicAPI =
        "newUser" :> ReqBody '[JSON] NewUser :> Post '[JSON] UserID
@@ -52,12 +52,15 @@ type SwaggerAPI = SwaggerSchemaUI "swagger-ui" "swagger.json"
 api :: Proxy API'
 api = Proxy
 
-type ServerAPI
-    =    BasicAuth "foo-realm" User :> PrivateAPI
-    :<|> PublicAPI -- :<|> SwaggerAPI
+type ServerAPI =  BasicAuth "foo-realm" User :> PrivateAPI :<|> PublicAPI -- :<|> SwaggerAPI
 
 
-type API
+type API =
     -- this serves both: swagger.json and swagger-ui
-    = SwaggerSchemaUI "swagger-ui" "swagger.json"
+    SwaggerSchemaUI "swagger-ui" "swagger.json"
     :<|> ServerAPI
+
+-- To test nested case
+type API' = API
+    :<|> "nested" :> API
+    :<|> SwaggerSchemaUI' "foo-ui" ("foo" :> "swagger.json" :> Get '[JSON] Swagger)
