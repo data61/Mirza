95a68e2924fa4208f1543605f26737fb60d55070
Sajid Ibne Anower <sajid.anower@data61.csiro.au>
2018-04-11 17:01:07 +1000

    Bugfix start scripts
    Former-commit-id: 58f5880bf8a48c9596581c4f381778c5a9e5f82e

--- manage_db.sh
+++ manage_db.sh
@@ -13,7 +13,6 @@ eof
     exit 1
 fi
 
-# FIXME: DB_NAME or DBNAME?
 DBNAME=$1
 
 psql \
@@ -29,9 +28,6 @@ if [ $psql_exit_status != 0 ]; then
     echo "psql failed while trying to run this sql script" 1>&2
     exit $psql_exit_status
 fi
-
-# @todo add some fake companies to the db to begin with
-# eventuially, we will get an updated list from ASIC and populate the db
 
 echo "sql script successful"
 exit 0
