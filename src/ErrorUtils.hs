46e164803b1a742cd545bea2373192762ba2017b
Sajid Ibne Anower <sajid.anower@data61.csiro.au>
2018-04-20 14:35:00 +1000

    Handle all service errors
    Former-commit-id: 998a06b87ab48464ba074a4a051c0e0c5a5b2d3a

--- src/ErrorUtils.hs
+++ src/ErrorUtils.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 
 -- | This module contains the helper functions that are used in error handling
 module ErrorUtils where
@@ -23,45 +25,75 @@ appErrToHttpErr (EmailExists _ (M.EmailAddress email)) =
   throwError $ err400 {
     errBody = LBSC8.fromChunks ["User email ", encodeUtf8 email, " exists."]
   }
-appErrToHttpErr (UnexpectedDBResponse _) =
-  throwError $ err500 {
-    errBody = "We received an unexpected response from our database. This error has been logged and someone is looking into it."
+appErrToHttpErr (InvalidKeyID _) =
+  throwError $ err400 {
+    errBody = "Invalid Key ID entered."
+  }
+appErrToHttpErr (NeedMoreSignatures _) =
+  throwError $ err400 {
+    errBody = "We need more signatures."
+  }
+appErrToHttpErr (InvalidSignature _) =
+  throwError $ err400 {
+    errBody = "Invalid Signature entered."
+  }
+appErrToHttpErr (InvalidEventID _) =
+  throwError $ err400 {
+    errBody = "Invalid Event ID entered."
+  }
+appErrToHttpErr (InvalidUserID _) =
+  throwError $ err400 {
+    errBody = "Invalid User ID entered."
+  }
+appErrToHttpErr (InvalidRSAKeyString _) =
+  throwError $ err400 {
+    errBody = "Invalid RSA Key entered."
+  }
+appErrToHttpErr (InvalidRSAKey _) =
+  throwError $ err400 {
+    errBody = "Invalid RSA Key entered."
+  }
+appErrToHttpErr (InvalidRSAKeySize _ _) =
+  throwError $ err400 {
+    errBody = "Invalid RSA Key entered."
+  }
+appErrToHttpErr (InvalidDigest _) =
+  throwError $ err400 {
+    errBody = "Invalid Key ID entered."
+  }
+appErrToHttpErr (AuthFailed (M.EmailAddress email)) =
+  throwError $ err403 {
+    errBody = LBSC8.fromChunks ["Authentication failed for email ", encodeUtf8 email, "."]
   }
 appErrToHttpErr (UserNotFound (M.EmailAddress email)) =
   throwError $ err404 {
     errBody = LBSC8.fromChunks ["User with email ", encodeUtf8 email, " could not be found."]
-  }
-appErrToHttpErr (AuthFailed (M.EmailAddress email)) =
-  throwError $ err404 {
-    errBody = LBSC8.fromChunks ["Authentication failed for email ", encodeUtf8 email, "."]
   }
 appErrToHttpErr (EmailNotFound (M.EmailAddress email)) =
   throwError $ err404 {
     errBody = LBSC8.fromChunks ["Email ", encodeUtf8 email, " could not be found."]
   }
 appErrToHttpErr (InsertionFail _ email) =
-  throwError $ err500 {
-    errBody = LBSC8.fromChunks ["Email ", encodeUtf8 email, " could not be inserted."]
-  }
-appErrToHttpErr (InvalidKeyID _) =
+  throw500Err $ LBSC8.fromChunks ["Email ", encodeUtf8 email, " could not be added. It might be worth trying again."]
+appErrToHttpErr (BlockchainSendFailed _) =
+  throw500Err "The data failed to send to the blockchain. We are looking into it. Thank you for your patience."
+appErrToHttpErr (ParseError _) =
   throwError $ err400 {
-    errBody = "Invalid Key ID entered."
+    errBody = "We could not parse the input provided."
+    -- ^ Add more information on what's wrong?
   }
-appErrToHttpErr (NeedMoreSignatures _) = generic500err
-appErrToHttpErr (InvalidSignature _) = generic500err
-appErrToHttpErr (BlockchainSendFailed _) = generic500err
-appErrToHttpErr (InvalidEventID _) = generic500err
-appErrToHttpErr (InvalidUserID _) = generic500err
-appErrToHttpErr (InvalidRSAKeyString _) = generic500err
-appErrToHttpErr (InvalidRSAKey _) = generic500err
-appErrToHttpErr (InvalidRSAKeySize _ _) = generic500err
-appErrToHttpErr (InvalidDigest _) = generic500err
-appErrToHttpErr (ParseError _) = generic500err
 appErrToHttpErr (BackendErr _) = generic500err
-appErrToHttpErr (DatabaseError _) = generic500err
+appErrToHttpErr (DatabaseError _) =
+  throw500Err "We received an unexpected response from our database. This error has been logged and someone is looking into it."
+appErrToHttpErr (UnexpectedDBResponse _) =
+  throw500Err "We received an unexpected response from our database. This error has been logged and someone is looking into it."
+-- TODO: The above error messages need to be more descriptive
 
 generic500err :: Handler a
 generic500err = throwError err500 {errBody = "The server did not understand this request."}
+
+throw500Err :: MonadError ServantErr m => LBSC8.ByteString -> m a
+throw500Err bdy = throwError err500 {errBody = bdy}
 
 -- TODO: Some of these might benefit from HasCallStack constraints
 
