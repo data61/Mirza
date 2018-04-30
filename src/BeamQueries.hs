150ae727d71f020d24909936875eee7f77c0fce8
Sajid Ibne Anower <sajid.anower@data61.csiro.au>
2018-04-30 15:25:48 +1000

    Add blockchain stubs back

--- src/BeamQueries.hs
+++ src/BeamQueries.hs
@@ -355,3 +355,39 @@ eventsByUser (M.UserID userId) = do
             SB.user_events_user_id userEvent ==. val_ (SB.UserId userId))
     pure (SB.json_event event)
   return $ catMaybes $ decodeEvent <$> eventList
+
+
+
+-- -- TODO - convert these below functions, and others in original file Storage.hs
+-- -- TODO = use EventId or EventID ???
+-- -- TODO = implement... there is no hash...
+-- createBlockchainPackage ::  (MonadError M.SigError m, MonadIO m) => EventId -> m C.BlockchainPackage
+
+
+-- createBlockchainPackage eventID = do
+--   -- XXX do we want to explicitly check that the hash is signed before assuming the first one is not?
+--   r <- liftIO $ query_ conn "SELECT hash, signedByUserID FROM Hashes WHERE eventID=? ORDER BY isSigned ASC;" $ Only eventID
+--   if length r > 2
+--     then
+--       let (plainHash, userID) = head r
+--           signatures = NonEmpty.fromList (map (\(s, u) -> ((M.Signature s), u)) (tail r))
+--       in
+--         return $ C.BlockchainPackage (M.EventHash plainHash) signatures
+--     else throwError M.SE_BlockchainSendFailed
+-- --TODO - Implement me
+-- -- sendToBlockchain ::  (MonadError M.SigError m, MonadIO m) =>  C.BlockchainPackage -> m ()
+-- sendToBlockchain :: Monad m => C.BlockchainPackage -> m ()
+-- sendToBlockchain package = return () -- if it fails, raise SE_SEND_TO_BLOCKCHAIN_FAILED error.
+
+-- checkSignature :: (MonadError M.SigError m, MonadIO m) => M.PEM_RSAPubKey -> ByteString.ByteString -> M.Signature -> m ()
+-- checkSignature pubkey blob signature =
+--   unless (C.verifySignature pubkey blob signature) $
+--     throwError M.SE_InvalidSignature
+-- -- TODO = use EventId or EventID
+-- -- ready to send to blockchain when all the parties have signed
+-- checkReadyForBlockchain :: (MonadError M.SigError m, MonadIO m) => Connection -> EventId -> m ()
+-- checkReadyForBlockchain eventID = do
+--   r <- liftIO $ query_ conn "SELECT COUNT(id) FROM UserEvents WHERE eventID=? AND hasSigned=FALSE;" $ Only eventID
+--   case r of
+--     [Only 0] -> pure ()
+--     _ -> throwError M.SE_NeedMoreSignatures
