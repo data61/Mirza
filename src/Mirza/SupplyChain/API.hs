6abdcb9ae12ddcc4792aaa67a9a746318ea71ef0
Sajid Ibne Anower <sajid.anower@data61.csiro.au>
2018-05-16 19:06:05 +1000

    Update GS1 snapshot, and make corresponding changes for compatibility
    Former-commit-id: 17af91e3adfbed136cf42c5407c0f1698d951e01

--- src/Mirza/SupplyChain/API.hs
+++ src/Mirza/SupplyChain/API.hs
@@ -15,7 +15,7 @@ import           Mirza.SupplyChain.Model       as M
 import           Mirza.SupplyChain.StorageBeam (PrimaryKeyType)
 
 import qualified Data.GS1.Event                as Ev
-import           Data.GS1.EventID
+import           Data.GS1.EventId
 
 import           Servant
 import           Servant.API.Flatten
@@ -24,20 +24,20 @@ import           Servant.Swagger.UI
 type PrivateAPI =
        "epc"      :> Capture "urn" M.LabelEPCUrn      :> "info"   :> Get '[JSON] EPCState
   :<|> "epc"      :> Capture "urn" M.LabelEPCUrn      :> "events" :> Get '[JSON] [Ev.Event]
-  :<|> "event"    :> Capture "eventID" EventID        :> "info"   :> Get '[JSON] (Maybe Ev.Event)
+  :<|> "event"    :> Capture "eventID" EventId        :> "info"   :> Get '[JSON] (Maybe Ev.Event)
   :<|> "contacts"                                                 :> Get '[JSON] [User]
   :<|> "contacts" :> "add"                 :> Capture "userID" M.UserID           :> Get '[JSON] Bool
   :<|> "contacts" :> "remove"              :> Capture "userID" M.UserID           :> Get '[JSON] Bool
   :<|> "contacts" :> "search"              :> Capture "term" String               :> Get '[JSON] [User]
   :<|> "event"    :> "list"                :> Capture "userID" M.UserID           :> Get '[JSON] [Ev.Event]
-  :<|> "event"    :> "listUsers"           :> Capture "eventID" EventID           :> Get '[JSON] [(User, Bool)]
+  :<|> "event"    :> "listUsers"           :> Capture "eventID" EventId           :> Get '[JSON] [(User, Bool)]
   :<|> "event"    :> "sign"                :> ReqBody '[JSON] SignedEvent         :> Post '[JSON] PrimaryKeyType
-  :<|> "event"    :> "getHash"             :> ReqBody '[JSON] EventID             :> Post '[JSON] HashedEvent
+  :<|> "event"    :> "getHash"             :> ReqBody '[JSON] EventId             :> Post '[JSON] HashedEvent
   :<|> "event"    :> "objectEvent"         :> ReqBody '[JSON] ObjectEvent         :> Post '[JSON] Ev.Event
   :<|> "event"    :> "aggregateEvent"      :> ReqBody '[JSON] AggregationEvent    :> Post '[JSON] Ev.Event
   :<|> "event"    :> "transactionEvent"    :> ReqBody '[JSON] TransactionEvent    :> Post '[JSON] Ev.Event
   :<|> "event"    :> "transformationEvent" :> ReqBody '[JSON] TransformationEvent :> Post '[JSON] Ev.Event
-  :<|> "event"    :> "addUser" :> Capture "userID" M.UserID :> Capture "eventID" EventID :> Post '[JSON] ()
+  :<|> "event"    :> "addUser" :> Capture "userID" M.UserID :> Capture "eventID" EventId :> Post '[JSON] ()
   :<|> "key"      :> "add"                 :> ReqBody '[JSON] PEM_RSAPubKey       :> Post '[JSON] KeyID
 
 type PublicAPI =
