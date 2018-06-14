6d3d328ce33d924d5023a312c79946a38ac22bd4
Sajid Ibne Anower <sajid.anower@data61.csiro.au>
2018-06-14 16:05:06 +1000

    Add basic revoke public key tests

--- test/Tests/Service.hs
+++ test/Tests/Service.hs
@@ -1,11 +1,12 @@
 {-# LANGUAGE DataKinds           #-}
 {-# LANGUAGE FlexibleContexts    #-}
 {-# LANGUAGE LambdaCase          #-}
+{-# LANGUAGE MonoLocalBinds      #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-
+{-# LANGUAGE TypeApplications    #-}
 
 module Tests.Service
-  ( testQueries
+  ( testServiceQueries
   ) where
 
 import           Tests.Dummies
@@ -22,10 +23,13 @@ import qualified Mirza.SupplyChain.StorageBeam                as SB
 import           Mirza.SupplyChain.Types
 
 import           Control.Monad                                (void)
+import           Data.Either                                  (isLeft)
 import           Data.Maybe                                   (fromJust,
                                                                isNothing)
+
 import qualified Data.Text                                    as T
 import           Data.Text.Encoding                           (encodeUtf8)
+
 import           Data.Time.Clock                              (getCurrentTime)
 import           Data.Time.LocalTime                          (LocalTime, utc,
                                                                utcToLocalTime)
@@ -51,8 +55,8 @@ testAppM scsContext act = runAppM scsContext act >>= \case
     Left err -> fail (show err)
     Right a -> pure a
 
-testQueries :: HasCallStack => SpecWith SCSContext
-testQueries = do
+testServiceQueries :: HasCallStack => SpecWith SCSContext
+testServiceQueries = do
 
   describe "addPublicKey tests" $
     it "addPublicKey test 1" $ \scsContext -> do
@@ -101,6 +105,35 @@ testQueries = do
           isNothing (revocationTime ki)
         )
 
+  describe "revokePublicKey tests" $ do
+    it "Revoke public key with permissions" $ \scsContext -> do
+      pubKey <- rsaPubKey
+      myKeyState <- testAppM scsContext $ do
+        uid <- newUser dummyNewUser
+        storageUser <- runDb $ getUserById uid
+        let user = userTableToModel . fromJust $ storageUser
+        keyId <- addPublicKey user pubKey Nothing
+        _timeKeyRevoked <- revokePublicKey user keyId
+        keyInfo <- getPublicKeyInfo keyId
+        pure (keyState keyInfo)
+      myKeyState `shouldBe` Revoked
+    it "Revoke public key without permissions" $ \scsContext -> do
+      pubKey <- rsaPubKey
+      r <- runAppM @_ @ServiceError scsContext $ do
+        uid <- newUser dummyNewUser
+        storageUser <- runDb $ getUserById uid
+        let user = userTableToModel . fromJust $ storageUser
+        keyId <- addPublicKey user pubKey Nothing
+
+        -- making a fake user
+        hackerUid <- newUser $ makeDummyNewUser (EmailAddress "l33t@hacker.com")
+        storageHacker <- runDb $ getUserById hackerUid
+        let hacker = userTableToModel . fromJust $ storageHacker
+
+        revokePublicKey hacker keyId
+      r `shouldSatisfy` isLeft
+      r `shouldBe` Left UnauthorisedKeyAccess
+
   describe "newUser tests" $
     it "newUser test 1" $ \scsContext -> do
       res <- testAppM scsContext $  do
@@ -136,7 +169,6 @@ testQueries = do
         user <- liftIO $ check basicAuthData
         pure (uid, user)
       case res of
-        -- (_, Nothing) -> fail "Received Nothing for user"
         (uid, Authorized user) ->
           user `shouldSatisfy`
             (\u ->
