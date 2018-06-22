ba08a30382b3e312e73ab1fbb75eb2aef308f66a
Ashley <ashley.stacey@data61.csiro.au>
2018-06-22 10:07:26 +1000

    Use Mirza.Common.Beam rather then duplicating shared code.
    Former-commit-id: 61fb586bdb0d8b37b405e8e0c202385e3df8ae43

--- src/Mirza/BusinessRegistry/MigrateScript.hs
+++ src/Mirza/BusinessRegistry/MigrateScript.hs
@@ -6,6 +6,7 @@
 module Mirza.BusinessRegistry.MigrateScript (migrationStorage) where
 
 import           Mirza.BusinessRegistry.StorageBeam
+import           Mirza.Common.Beam
 
 import qualified Data.GS1.EPC                         as EPC
 
@@ -75,49 +76,6 @@ migrationStorage () =
             (field "biz_lat" double)
             (field "biz_long" double)
       )
-
--- TODO: Move this to common module for both servers
-
--- | The generic implementation of fromField
--- If it's a fromField used for ``SomeCustomType``, sample usage would be
--- instance FromField SomeCustomType where
---   fromField = defaultFromField "SomeCustomType"
-defaultFromField :: (B.Typeable b, Read b) => String
-                 -> Field
-                 -> Maybe ByteString
-                 -> Conversion b
-defaultFromField fName f bs = do
-  x <- readMaybe <$> fromField f bs
-  case x of
-    Nothing ->
-      returnError ConversionFailed
-        f $ "Could not 'read' value for " ++ fName
-    Just val -> pure val
-
-
--- | Shorthand for using postgres text type
-textType :: BMigrate.DataType PgDataTypeSyntax a
-textType = BMigrate.DataType pgTextType
-
-
--- | Helper function to manage the returnValue of ``readMaybe`` or gracefully
--- fail
-handleReadColumn :: Monad f => Maybe a -> String -> String -> f a
-handleReadColumn (Just a) _ _= pure a
-handleReadColumn Nothing colName val =
-    fail ("Invalid value for " ++ colName ++ ": " ++ val)
-
--- defaultFromBackendRow :: String
---                       -> F (FromBackendRowF be) b
--- | Wrapper that calls read and fail appropriately
--- An explicit definition of ``fromBackendRow`` is required for each custom type
-defaultFromBackendRow :: (Read a, BSQL.FromBackendRow be T.Text)
-                      => String -> BSQL.FromBackendRowM be a
-defaultFromBackendRow colName = do
-  val <- BSQL.fromBackendRow
-  let valStr = T.unpack val
-  handleReadColumn (readMaybe valStr) colName valStr
-
 
 -- ======= EPC.GS1CompanyPrefix =======
 
