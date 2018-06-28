96e6554e1fc44096a5c0e659f09c2c54f974e0a9
Ashley <ashley.stacey@data61.csiro.au>
2018-06-28 16:08:53 +1000

    Add interactive migration.
    Co-authored-by: Alex Mason <Alex.Mason@data61.csiro.au>
    
    Former-commit-id: 3b8a30c202e9eb061fa790612261d2733017e8ef

--- src/Mirza/BusinessRegistry/Database/Schema.hs
+++ src/Mirza/BusinessRegistry/Database/Schema.hs
@@ -1,25 +1,38 @@
-
+{-# LANGUAGE TypeApplications #-}
 
 module Mirza.BusinessRegistry.Database.Schema
   ( module Current
-  , migration, businessRegistryDB ) where
-
-import qualified Mirza.BusinessRegistry.Database.Schema.V0001 as Current
-
-import qualified Mirza.BusinessRegistry.Database.Schema.V0001 as V0001 (businessRegistryDB,
-                                                                        migration)
--- import qualified Mirza.BusinessRegistry.Database.Schema.V0002 as V0002 (businessRegistryDB, migration)
+    , migration
+    , businessRegistryDB ) where
 
 -- import           Control.Arrow ((>>>))
 
+import qualified Data.ByteString.Lazy.Char8                   as BSL
+
+import           Control.Monad.IO.Class                       (liftIO)
+
+import           Control.Lens                                 (view, _1)
+
 import           Database.Beam                                (DatabaseSettings)
-import           Database.Beam.Migrate.Types                  (CheckedDatabaseSettings,
-                                                               MigrationSteps,
-                                                               evaluateDatabase,
-                                                               migrationStep,
-                                                               unCheckDatabase)
-import           Database.Beam.Postgres                       (PgCommandSyntax,
+import           Database.Beam.Backend                        (runNoReturn)
+import           Database.Beam.Migrate.Simple                 (runSimpleMigration,
+                                                               simpleMigration)
+import           Database.Beam.Migrate.Types                  hiding
+                                                               (migrateScript)
+import           Database.Beam.Postgres                       (Pg,
+                                                               PgCommandSyntax,
                                                                Postgres)
+import           Database.Beam.Postgres.Migrate               (migrateScript,
+                                                               migrationBackend)
+
+
+import           Mirza.Common.Types
+
+import           Mirza.BusinessRegistry.Database.Schema.V0001 as Current hiding (migration)
+
+import qualified Mirza.BusinessRegistry.Database.Schema.V0001 as V0001 (migration)
+-- import qualified Mirza.BusinessRegistry.Database.Schema.V0002 as V0002 (businessRegistryDB, migration)
+
 
 migration :: MigrationSteps PgCommandSyntax () (CheckedDatabaseSettings Postgres Current.BusinessRegistryDB)
 migration = migrationStep "Initial commit" V0001.migration
@@ -30,3 +43,28 @@ businessRegistryDB = unCheckDatabase checkedBusinessRegistryDB
 
 checkedBusinessRegistryDB :: CheckedDatabaseSettings Postgres Current.BusinessRegistryDB
 checkedBusinessRegistryDB = evaluateDatabase migration
+
+
+
+-- TODO: Use autoMigrate if possible and confirm with the user whether to do dangerous migrations explicitly
+-- TODO: Move this into Mirza.Common.Beam
+runMigration context = do
+   mapM_ BSL.putStr $ migrateScript migration
+   putStrLn "type YES to confirm applying this migration:"
+   confirm <- getLine
+   case confirm of
+    "YES" -> runAppM context $ runDb $ do
+      -- simpleMigration - needs Connection, postgresmigrationBackend, checked migration
+          conn <- view _1
+          mcommands <- liftIO $ simpleMigration migrationBackend conn checkedBusinessRegistryDB
+          case mcommands of
+            Nothing -> fail "lol"
+            Just commands -> do
+              -- TODO: Print these commands as they're just the changes needed
+              liftIO $ runSimpleMigration
+                          @PgCommandSyntax
+                          @Postgres
+                          @_
+                          @Pg
+                          conn commands
+    _ -> fail "Ok, not migrating"
