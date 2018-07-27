9c42a88cf586cd912257d55e5c56cd4107b22b2e
Sajid Ibne Anower <sajid.anower@data61.csiro.au>
2018-07-27 17:04:20 +1000

    Fix dummies

--- test/Mirza/BusinessRegistry/Tests/Dummies.hs
+++ test/Mirza/BusinessRegistry/Tests/Dummies.hs
@@ -6,16 +6,17 @@ module Mirza.BusinessRegistry.Tests.Dummies where
 -- import           Mirza.SupplyChain.Types hiding (InProgress, NewUser (..))
 -- import qualified Mirza.SupplyChain.Types as ST
 
-import           Mirza.Common.Types as CT
+import           Data.GS1.EPC                 (GS1CompanyPrefix (..))
+import           Mirza.BusinessRegistry.Types as BRT
+import           Mirza.Common.Types           as CT
 
-import           Data.GS1.EPC       (GS1CompanyPrefix (..))
 -- add function to generate and take dummyLabelEpc
 
 -- General Utils
-dummyNewUser :: CT.NewUser
+dummyNewUser :: BRT.NewUser
 dummyNewUser = makeDummyNewUser (EmailAddress "fake@gmail.com")
 
 -- | Utility function to make many users on the fly
-makeDummyNewUser :: CT.EmailAddress -> CT.NewUser
+makeDummyNewUser :: CT.EmailAddress -> BRT.NewUser
 makeDummyNewUser emailAddress =
-    CT.NewUser "000" emailAddress "Bob" "Smith" (GS1CompanyPrefix "blah Ltd") "password"
+    BRT.NewUser "000" emailAddress "Bob" "Smith" (GS1CompanyPrefix "blah Ltd") "password"
