6592cf639c6e40d62b162ac0d83af50ef624c23a
Sajid Ibne Anower <sajid.anower@data61.csiro.au>
2018-08-10 12:16:40 +1000

    Add comment
    Former-commit-id: 6a31ba3a2c8fbbf634c7dafe9a967bad9701facf

--- runTests.sh
+++ runTests.sh
@@ -1,17 +1,18 @@
 #! /usr/bin/env sh
 
-export test_command="stack test --coverage --fast --ta '$@' -j1"
-
 stack test --coverage --fast --ta "$@" -j1
 
+# Keep the commented out code. It's helpful if we want to make an option
+# to open up the test report after running the tests 
+
 # The line where the link is starts with these phrases
-export report_link_header="An index of the generated HTML coverage reports is available at "
+# export report_link_header="An index of the generated HTML coverage reports is available at "
 
-test_report=`stack test --coverage --fast --ta "$@" -j1 2>&1 | 
-             egrep -i "$report_link_header" |
-             sed "s/$report_link_header//g"`
+# test_report=`stack test --coverage --fast --ta "$@" -j1 2>&1 | 
+#              egrep -i "$report_link_header" |
+#              sed "s/$report_link_header//g"`
 
-echo "Your report lives in $test_report"
+# echo "Your report lives in $test_report"
 
 # Wouldn't work on macOS
 # uname -a | egrep -i 'Linux' && sensible-browser $test_report
