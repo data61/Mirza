027678ce6d710e66e2f9ff1544d10b6643eee91d
Sajid Anower (Data61, Eveleigh ATP) <Sajid.Anower@data61.csiro.au>
2018-08-13 15:48:52 +1000

    Delete redundant files; Closes #187 (#194)

--- src/Mirza/SupplyChain/Handlers/Signatures.hs
+++ src/Mirza/SupplyChain/Handlers/Signatures.hs
@@ -147,7 +147,6 @@ insertSignature eId kId (Signature sig) digest = do
 
 
 -- do we need this?
---
 eventHashed :: ST.User -> EvId.EventId -> AppM context err HashedEvent
 eventHashed _user _eventId = error "not implemented yet"
 -- return (HashedEvent eventId (EventHash "Blob"))
@@ -159,3 +158,40 @@ eventHashed user eventId = do
     Nothing -> throwError err404 { errBody = "Unknown eventId" }
     Just i -> return i
 -}
+
+-- BeamQueries residue
+
+-- -- TODO - convert these below functions, and others in original file Storage.hs
+-- -- TODO = use EventId or EventId ???
+-- -- TODO = implement... there is no hash...
+-- createBlockchainPackage ::  (MonadError SigError m, MonadIO m) => EventId -> m C.BlockchainPackage
+
+
+-- createBlockchainPackage eventID = do
+--   -- XXX do we want to explicitly check that the hash is signed before assuming the first one is not?
+--   r <- liftIO $ query_ conn "SELECT hash, signedByUserID FROM Hashes WHERE eventID=? ORDER BY isSigned ASC;" $ Only eventID
+--   if length r > 2
+--     then
+--       let (plainHash, userID) = head r
+--           signatures = NonEmpty.fromList (map (\(s, u) -> ((Signature s), u)) (tail r))
+--       in
+--         return $ C.BlockchainPackage (EventHash plainHash) signatures
+--     else throwError SE_BlockchainSendFailed
+-- --TODO - Implement me
+-- -- sendToBlockchain ::  (MonadError SigError m, MonadIO m) =>  C.BlockchainPackage -> m ()
+-- sendToBlockchain :: Monad m => C.BlockchainPackage -> m ()
+-- sendToBlockchain package = return () -- if it fails, raise SE_SEND_TO_BLOCKCHAIN_FAILED error.
+
+-- checkSignature :: (MonadError SigError m, MonadIO m) => PEM_RSAPubKey -> ByteString.ByteString -> Signature -> m ()
+-- checkSignature pubkey blob signature =
+--   unless (C.verifySignature pubkey blob signature) $
+--     throwError SE_InvalidSignature
+-- -- TODO = use EventId or EventId
+-- -- ready to send to blockchain when all the parties have signed
+-- checkReadyForBlockchain :: (MonadError SigError m, MonadIO m) => Connection -> EventId -> m ()
+-- checkReadyForBlockchain eventID = do
+--   r <- liftIO $ query_ conn "SELECT COUNT(id) FROM UserEvents WHERE eventID=? AND hasSigned=FALSE;" $ Only eventID
+--   case r of
+--     [Only 0] -> pure ()
+--     _ -> throwError SE_NeedMoreSignatures
+
