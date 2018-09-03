36c740c9173deb43a954a9733869082eb9ad2f39
Alex Mason <Alex.Mason@data61.csiro.au>
2018-09-03 16:14:46 +1000

    Pull out common code to run Wai app in tests, clean up some imports. (#225)
    Former-commit-id: 990fb91c96c804fa089bef21f5a9e911f90352d1

--- test/Mirza/BusinessRegistry/Tests/Client.hs
+++ test/Mirza/BusinessRegistry/Tests/Client.hs
@@ -1,15 +1,9 @@
 module Mirza.BusinessRegistry.Tests.Client where
 
 import           Mirza.BusinessRegistry.Tests.Settings (testDbConnStr)
+import           Mirza.Common.Test.ServantUtil
 
-import           Control.Concurrent                    (ThreadId, forkIO,
-                                                        killThread)
-import           System.IO.Unsafe                      (unsafePerformIO)
-
-import qualified Network.HTTP.Client                   as C
-import           Network.Socket
-import qualified Network.Wai                           as Wai
-import           Network.Wai.Handler.Warp
+import           Control.Concurrent                    (ThreadId)
 
 import           Servant.API.BasicAuth
 import           Servant.Client
@@ -29,7 +23,6 @@ import           Data.GS1.EPC                          (GS1CompanyPrefix (..))
 
 import           Katip                                 (Severity (DebugS))
 
--- Cribbed from https://github.com/haskell-servant/servant/blob/master/servant-client/test/Servant/ClientSpec.hs
 
 -- === Servant Client tests
 
@@ -59,29 +52,3 @@ clientSpec =
   afterAll endWaiApp $ do
     it "Stub" $ \(_,_baseurl) -> do
       pending
-
-startWaiApp :: Wai.Application -> IO (ThreadId, BaseUrl)
-startWaiApp app = do
-    (prt, sock) <- openTestSocket
-    let settings = setPort prt defaultSettings
-    thread <- forkIO $ runSettingsSocket settings sock app
-    return (thread, BaseUrl Http "localhost" prt "")
-
-endWaiApp :: (ThreadId, BaseUrl) -> IO ()
-endWaiApp (thread, _) = killThread thread
-
-openTestSocket :: IO (Port, Socket)
-openTestSocket = do
-  s <- socket AF_INET Stream defaultProtocol
-  localhost <- inet_addr "127.0.0.1"
-  bind s (SockAddrInet aNY_PORT localhost)
-  listen s 1
-  prt <- socketPort s
-  return (fromIntegral prt, s)
-
-{-# NOINLINE manager' #-}
-manager' :: C.Manager
-manager' = unsafePerformIO $ C.newManager C.defaultManagerSettings
-
-runClient :: ClientM a -> BaseUrl -> IO (Either ServantError a)
-runClient x baseUrl' = runClientM x (mkClientEnv manager' baseUrl')
