1731f4df4246abe7d077131c9155e261f0375c4c
Alex Mason <Alex.Mason@data61.csiro.au>
2018-09-18 15:25:20 +1000

    Add check to ensure that user deleting a location is a member of this biz
    Former-commit-id: e536a458066921b1ff298d913cb5f521b464fbe4

--- src/Mirza/BusinessRegistry/Handlers/Location.hs
+++ src/Mirza/BusinessRegistry/Handlers/Location.hs
@@ -11,23 +11,21 @@ module Mirza.BusinessRegistry.Handlers.Location
   ) where
 
 
-import           Mirza.BusinessRegistry.Database.Schema as DB
-import           Mirza.BusinessRegistry.Handlers.Common
+import           Mirza.BusinessRegistry.Database.Schema   as DB
 import           Mirza.BusinessRegistry.Types             as BT
 import           Mirza.Common.Utils
 import           Mirza.Common.Types                       (Member)
 
-import           Data.GS1.EPC                             as EPC
-
 import           Database.Beam                            as B
 import           Database.Beam.Backend.SQL.BeamExtensions
-import           Database.PostgreSQL.Simple.Errors        (ConstraintViolation (UniqueViolation),
-                                                           constraintViolation)
+-- import           Database.PostgreSQL.Simple.Errors        (ConstraintViolation (UniqueViolation),
+--                                                            constraintViolation)
 
-import           Control.Lens                             ((^?))
+import           Control.Lens                             ((#))
 
 import           GHC.Stack                                (HasCallStack, callStack)
-import           Control.Monad.Except                     (catchError, throwError)
+-- import           Control.Monad.Except                     (catchError, throwError)
+import           Control.Monad.Error.Hoist                (hoistErrorM)
 
 
 import           Servant (NoContent(NoContent))
@@ -92,17 +90,31 @@ newLocationToLocation (LocationId locId) bizId NewLocation {newLocGLN, newLocCoo
     }
 
 removeLocation :: (Member context '[HasEnvType, HasConnPool, HasLogging]
-                  , Member err    '[AsSqlError]) 
+                  , Member err    '[AsSqlError, AsBusinessRegistryError]) 
                => AuthUser
                -> LocationId
                -> AppM context err NoContent
 removeLocation auser locId = NoContent <$ runDb (removeLocationQuery auser locId)
 
 removeLocationQuery :: (Member context '[]
-                      , Member err     '[]) 
+                      , Member err     '[AsBusinessRegistryError]) 
                     => AuthUser
                     -> LocationId
-                    -> DB context err ()
-removeLocationQuery auser (LocationId locId) = 
-  pg $ runDelete $ delete (_locations businessRegistryDB) 
-                          (\loc -> location_id loc ==. val_ locId)
+                    -> DB context err [Location]
+removeLocationQuery (AuthUser (BT.UserId auser)) (LocationId locId) = do
+  _ <- hoistErrorM (_LocationRemovalErrorBRE #) $
+        pg $ runSelectReturningOne $ select $ do
+          user <- all_ (_users businessRegistryDB)
+          loc  <- all_ (_locations businessRegistryDB)
+          guard_ (user_id user ==. val_ auser
+                  &&. location_id loc ==. val_ locId
+                  &&. user_biz_id user ==. location_biz_id loc)
+          pure user
+
+      -- TODO: Check that user is member of business owning location
+       -- see Database.Beam.Backend.SQL.BeamExtensions.runDeleteReturningList
+       -- and subquery_ 
+
+  pg $ runDeleteReturningList 
+        (_locations businessRegistryDB) 
+        (\loc -> location_id loc ==. val_ locId)
