c807d5d4bcd1c1b000f65dd002c86645415ea92a
Sajid Ibne Anower <sajidanower23@gmail.com>
2018-10-10 10:53:11 +1100

    Use unsafeMkEmailAddress to construct email address
    Former-commit-id: 50e5f64318eb768348bdd20a92b27900ee57b9ea

--- test/Mirza/BusinessRegistry/Tests/Dummies.hs
+++ test/Mirza/BusinessRegistry/Tests/Dummies.hs
@@ -6,14 +6,13 @@ import           Data.GS1.EPC                 (GS1CompanyPrefix (..))
 import           Mirza.BusinessRegistry.Types as BRT
 import           Mirza.Common.Types           as CT
 
-import           Data.Maybe                   (fromJust)
+import           Mirza.Common.Tests.Utils     (unsafeMkEmailAddress)
 
-import           Text.Email.Validate          (emailAddress)
 dummyBusiness :: BRT.NewBusiness
 dummyBusiness = NewBusiness (GS1CompanyPrefix "3000001") "pubKeyTests_businessName"
 
 dummyNewUser :: BRT.NewUser
-dummyNewUser = makeDummyNewUser (fromJust $ emailAddress "fake@gmail.com")
+dummyNewUser = makeDummyNewUser (unsafeMkEmailAddress "fake@gmail.com")
 
 -- | Utility function to make many users on the fly
 makeDummyNewUser :: CT.EmailAddress -> BRT.NewUser
