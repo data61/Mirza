d497a659357afa14750b039412cba8b1880636e9
Sara Falamaki <Sara.Falamaki@csiro.au>
2018-11-08 15:45:14 +1100

    Moved the comments for tests into appropriate test modules Added test module for blockchain tests

---
+++ test/Mirza/SupplyChain/Tests/Blockchain.hs
@@ -0,0 +1,15 @@
+module Mirza.SupplyChain.Tests.Blockchain where
+
+{-
+
+Tests that should be implemented here
+
+  Check for tampering by comparing to Blockchain hash
+eventInfo ← eventInfo(eventID)
+joseTxt = joseText eventInfo
+expectedHash = hash joseText
+blockchainID = blockchainID eventInfo
+bcHash = getBlockchainHash(blockchainID)
+assert (bcHash == expectedHash)
+
+-}
