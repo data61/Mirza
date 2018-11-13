ca18de89cb80fe53a0473e36b268d73240a6e084
Sajid Ibne Anower <sajidanower23@gmail.com>
2018-11-13 19:46:55 +1100

    Take basicauthdata in before adding users with BRClient; Closes #301

--- test/Mirza/BusinessRegistry/Tests/Generate.hs
+++ test/Mirza/BusinessRegistry/Tests/Generate.hs
@@ -42,6 +42,12 @@ genNUsersBR :: TestName -> Int -> [NewUser]
 genNUsersBR _ 0        = []
 genNUsersBR testName n = mkNewUserByNumber testName n : genNUsersBR testName (n - 1)
 
+globalCompanyPrefix :: GS1CompanyPrefix
+globalCompanyPrefix = GS1CompanyPrefix "1234567"
+
+globalBusiness :: NewBusiness
+globalBusiness = NewBusiness globalCompanyPrefix "Generator Business Ltd."
+
 mkNewUserByNumber :: String -> Int -> NewUser
 mkNewUserByNumber testName n =
   let numStr = testName ++ "_" ++ show n
@@ -53,34 +59,39 @@ mkNewUserByNumber testName n =
   , newUserEmailAddress = unsafeMkEmailAddress $ BS.concat ["abc", numBS, "@example.com"]
   , newUserFirstName = T.append "First: " numT
   , newUserLastName = T.append "Last: " numT
-  , newUserCompany = GS1CompanyPrefix $ T.append "671456___" numT
+  , newUserCompany = globalCompanyPrefix -- Company prefix is constant
   , newUserPassword = "re4lly$ecret14!"}
 
 
-insertNUsersSCS :: TestName
-                -> Int
-                -> [ClientM UserId]
-insertNUsersSCS testName n =
+insertNUsersBR :: TestName
+               -> Int
+               -> BasicAuthData
+               -> [ClientM UserId]
+insertNUsersBR testName n brAuthData =
+
   let users = genNUsersBR testName n
   in
-    BRClient.addUser <$> users
+    BRClient.addUser brAuthData <$> users
 
 type Firstname = T.Text
 
--- Insert multiple users into the SCS DB given a
+-- Insert multiple users into the BR DB given a
 -- list of first names and company prefixes.
-insertMultipleUsersBR  :: TestName
-                        -> [Firstname]
-                        -> [GS1CompanyPrefix]
-                        -> [ClientM UserId]
-insertMultipleUsersBR name fn pfx =
-  BRClient.addUser <$> genMultipleUsersBR n name fn pfx
+insertMultipleUsersBR :: TestName
+                      -> BasicAuthData
+                      -> [Firstname]
+                      -> [GS1CompanyPrefix]
+                      -> [ClientM UserId]
+insertMultipleUsersBR testName brAuthData fn pfx =
+  BRClient.addUser brAuthData <$> genMultipleUsersBR n testName fn pfx
   where
     n = min (length fn) (length pfx)
 
-
-genMultipleUsersBR :: Int ->  TestName -> [Firstname] ->
-    [GS1CompanyPrefix] -> [NewUser]
+genMultipleUsersBR :: Int
+                   -> TestName
+                   -> [Firstname]
+                   -> [GS1CompanyPrefix]
+                   -> [NewUser]
 genMultipleUsersBR 0 _ _ _ = []
 genMultipleUsersBR _ _ [] _ = []
 genMultipleUsersBR _ _ _ [] = []
