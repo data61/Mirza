78989d3c7d3b4225bb4595bf4c788c73cfa56b2f
Sajid Ibne Anower <sajidanower23@gmail.com>
2018-11-13 19:13:54 +1100

    Fix prefixes

--- test/Mirza/BusinessRegistry/Tests/Generate.hs
+++ test/Mirza/BusinessRegistry/Tests/Generate.hs
@@ -4,25 +4,23 @@
 -- each service's client
 module Mirza.BusinessRegistry.Tests.Generate where
 
--- import           Mirza.BusinessRegistry.Types      as BT
+import           Mirza.BusinessRegistry.Types          as BT
 
-import           Mirza.SupplyChain.Types          as ST
+import           Data.GS1.EPC                          (GS1CompanyPrefix (..))
 
-import           Data.GS1.EPC                     (GS1CompanyPrefix (..))
+import qualified Data.Text                             as T
+import           Data.Text.Encoding                    (encodeUtf8)
 
-import qualified Data.Text                        as T
-import           Data.Text.Encoding               (encodeUtf8)
+import qualified Data.ByteString.Char8                 as BS
 
-import qualified Data.ByteString.Char8            as BS
+import           Mirza.BusinessRegistry.Client.Servant as BRClient
 
-import           Mirza.SupplyChain.Client.Servant as SCSClient
+import           Servant.API.BasicAuth                 (BasicAuthData (..))
 
-import           Servant.API.BasicAuth            (BasicAuthData (..))
+import           Mirza.Common.Tests.Utils              (unsafeMkEmailAddress)
+import           Text.Email.Validate                   (toByteString)
 
-import           Mirza.Common.Tests.Utils         (unsafeMkEmailAddress)
-import           Text.Email.Validate              (toByteString)
-
-import           Servant.Client                   (ClientM)
+import           Servant.Client                        (ClientM)
 
 type TestName = String
 
@@ -46,23 +44,23 @@ authFirstUser = BasicAuthData
   (encodeUtf8   . newUserPassword     $ firstUser)
 
 
-genNUsersSCS :: TestName -> Int -> [ST.NewUser]
+genNUsersSCS :: TestName -> Int -> [NewUser]
 genNUsersSCS _ 0        = []
 genNUsersSCS testName n = mkNewUserByNumber testName n : genNUsersSCS testName (n - 1)
 
-mkNewUserByNumber :: String -> Int -> ST.NewUser
+mkNewUserByNumber :: String -> Int -> NewUser
 mkNewUserByNumber testName n =
   let numStr = testName ++ "_" ++ show n
       numT = T.pack numStr
       numBS = BS.pack numStr
   in
-  ST.NewUser
-  { ST.newUserPhoneNumber = T.append "0400 111 22" numT
-  , ST.newUserEmailAddress = unsafeMkEmailAddress $ BS.concat ["abc", numBS, "@example.com"]
-  , ST.newUserFirstName = T.append "First: " numT
-  , ST.newUserLastName = T.append "Last: " numT
-  , ST.newUserCompany = GS1CompanyPrefix $ T.append "671456___" numT
-  , ST.newUserPassword = "re4lly$ecret14!"}
+  NewUser
+  { newUserPhoneNumber = T.append "0400 111 22" numT
+  , newUserEmailAddress = unsafeMkEmailAddress $ BS.concat ["abc", numBS, "@example.com"]
+  , newUserFirstName = T.append "First: " numT
+  , newUserLastName = T.append "Last: " numT
+  , newUserCompany = GS1CompanyPrefix $ T.append "671456___" numT
+  , newUserPassword = "re4lly$ecret14!"}
 
 
 insertNUsersSCS :: TestName
@@ -71,39 +69,36 @@ insertNUsersSCS :: TestName
 insertNUsersSCS testName n =
   let users = genNUsersSCS testName n
   in
-    SCSClient.addUser <$> users
+    BRClient.addUser <$> users
 
 type Firstname = T.Text
 
 -- Insert multiple users into the SCS DB given a
 -- list of first names and company prefixes.
-insertMultipleUsersSCS  :: TestName
+insertMultipleUsersBR  :: TestName
                         -> [Firstname]
                         -> [GS1CompanyPrefix]
                         -> [ClientM UserId]
-insertMultipleUsersSCS name fn pfx =
-  SCSClient.addUser <$> genMultipleUsersSCS n name fn pfx
+insertMultipleUsersBR name fn pfx =
+  BRClient.addUser <$> genMultipleUsersBR n name fn pfx
   where
     n = min (length fn) (length pfx)
 
 
-genMultipleUsersSCS :: Int ->  TestName -> [Firstname] ->
-    [GS1CompanyPrefix] -> [ST.NewUser]
-genMultipleUsersSCS 0 _ _ _ = []
-genMultipleUsersSCS _ _ [] _ = []
-genMultipleUsersSCS _ _ _ [] = []
-genMultipleUsersSCS n testName (f:fx) (p:px) =
-  newUser : genMultipleUsersSCS (n-1) testName fx px
+genMultipleUsersBR :: Int ->  TestName -> [Firstname] ->
+    [GS1CompanyPrefix] -> [NewUser]
+genMultipleUsersBR 0 _ _ _ = []
+genMultipleUsersBR _ _ [] _ = []
+genMultipleUsersBR _ _ _ [] = []
+genMultipleUsersBR n testName (f:fx) (p:px) =
+  newUser : genMultipleUsersBR (n-1) testName fx px
   where
     numT = T.pack $ show n
-    newUser = ST.NewUser
-      { ST.newUserPhoneNumber = T.append "0400 111 22" numT
-      , ST.newUserEmailAddress =
+    newUser = NewUser
+      { newUserPhoneNumber = T.append "0400 111 22" numT
+      , newUserEmailAddress =
           unsafeMkEmailAddress $ BS.concat [encodeUtf8 f, "@example.com"]
-      , ST.newUserFirstName = f
-      , ST.newUserLastName = T.append "Last: " numT
-      , ST.newUserCompany = p
-      , ST.newUserPassword = "re4lly$ecret14!"}
-
-
--- insertMultipleUsersBR ::
+      , newUserFirstName = f
+      , newUserLastName = T.append "Last: " numT
+      , newUserCompany = p
+      , newUserPassword = "re4lly$ecret14!"}
