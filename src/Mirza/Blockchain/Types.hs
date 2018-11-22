11cedc4bb6409289b26e05b65a70571d6979b407
Sajid Ibne Anower <sajidanower23@gmail.com>
2018-11-22 14:25:16 +1100

    Clean up imports
    Former-commit-id: 5560719929129376b49ebee3582743bc8fa189cd

--- src/Mirza/Blockchain/Types.hs
+++ src/Mirza/Blockchain/Types.hs
@@ -10,42 +10,15 @@ module Mirza.Blockchain.Types
   )
   where
 
-import           Mirza.Common.Types         as Common
+import           Mirza.Common.Types as Common
 
-import           Data.GS1.DWhat
-import           Data.GS1.DWhen
-import           Data.GS1.DWhere
-import           Data.GS1.DWhy
-import           Data.GS1.EPC               as EPC
-import qualified Data.GS1.Event             as Ev
-import           Data.GS1.EventId           as EvId
-
-import           Database.PostgreSQL.Simple (Connection, SqlError)
-
-import           Crypto.JOSE                as JOSE hiding (Digest)
-import           Crypto.JOSE.Types          (Base64Octets)
-import           Crypto.Scrypt              (ScryptParams)
-
-import           Servant                    (FromHttpApiData, ToHttpApiData)
-import           Servant.Client             (ClientEnv (..), ServantError (..))
-
-import           Control.Lens
-
-import           GHC.Generics               (Generic)
+import           GHC.Generics       (Generic)
 
 import           Data.Aeson
 import           Data.Aeson.TH
-import           Data.Aeson.Types
-import qualified Data.ByteString            as BS
-import           Data.List.NonEmpty         (NonEmpty)
-import           Data.Pool                  as Pool
+
 import           Data.Swagger
-import           Data.Text                  (Text)
-
-import           Katip                      as K
-
-import           Data.Time                  (UTCTime)
-
+import           Servant            (FromHttpApiData, ToHttpApiData)
 
 
 import           Data.Text          (Text)
@@ -54,27 +27,28 @@ import           Data.Time          (UTCTime)
 
 newtype BlockchainTransactionHash = BlockchainTransactionHash Text
   deriving (Eq, Show, Generic)
-
 $(deriveJSON defaultOptions ''BlockchainTransactionHash)
 instance ToSchema BlockchainTransactionHash
-
+deriving instance FromHttpApiData BlockchainTransactionHash
+deriving instance ToHttpApiData BlockchainTransactionHash
 
 newtype IdentifierHash = IdentifierHash Text
   deriving (Eq, Show, Generic)
-
 $(deriveJSON defaultOptions ''IdentifierHash)
 instance ToSchema IdentifierHash
+deriving instance FromHttpApiData IdentifierHash
+deriving instance ToHttpApiData IdentifierHash
 
 newtype EventHash = EventHash Text
   deriving (Eq, Show, Generic)
-
 $(deriveJSON defaultOptions ''EventHash)
 instance ToSchema EventHash
+deriving instance FromHttpApiData EventHash
+deriving instance ToHttpApiData EventHash
 
 data EventInsertionResponse = EventInsertionResponse
   { respEventHash :: EventHash
   , respTimeStamp :: UTCTime
   } deriving (Eq, Show, Generic)
-
 $(deriveJSON defaultOptions ''EventInsertionResponse)
 instance ToSchema EventInsertionResponse
