82b635f838e445441d03883020b745a10aa3c5e7
Sajid Ibne Anower <sajidanower23@gmail.com>
2018-11-22 16:00:28 +1100

    Define instances by hand
    Former-commit-id: 9d213d3ca7be68b837d92e98c1b8e8cb90796f6b

--- src/Mirza/Blockchain/Types.hs
+++ src/Mirza/Blockchain/Types.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE DeriveGeneric              #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE StandaloneDeriving         #-}
 {-# LANGUAGE TemplateHaskell            #-}
 {-# OPTIONS_GHC -Wno-orphans            #-}
 
@@ -14,41 +13,77 @@ import           Mirza.Common.Types as Common
 
 import           GHC.Generics       (Generic)
 
-import           Data.Aeson
+import           Data.Aeson         (FromJSON (..), ToJSON (..))
 import           Data.Aeson.TH
 
 import           Data.Swagger
-import           Servant            (FromHttpApiData, ToHttpApiData)
+import           Servant            (FromHttpApiData (..), ToHttpApiData (..))
 
 
 import           Data.Text          (Text)
 
+import           Data.Text.Encoding (decodeUtf8, encodeUtf8)
 import           Data.Time          (UTCTime)
 
-newtype BlockchainTransactionHash = BlockchainTransactionHash Text
-  deriving (Eq, Show, Generic)
-$(deriveJSON defaultOptions ''BlockchainTransactionHash)
-instance ToSchema BlockchainTransactionHash
-deriving instance FromHttpApiData BlockchainTransactionHash
-deriving instance ToHttpApiData BlockchainTransactionHash
+import           Data.Proxy         (Proxy (..))
 
-newtype IdentifierHash = IdentifierHash Text
-  deriving (Eq, Show, Generic)
-$(deriveJSON defaultOptions ''IdentifierHash)
-instance ToSchema IdentifierHash
-deriving instance FromHttpApiData IdentifierHash
-deriving instance ToHttpApiData IdentifierHash
+import           Control.Lens
 
-newtype EventHash = EventHash Text
+import           Data.ByteString    (ByteString)
+
+newtype BlockchainTransactionHash = BlockchainTransactionHash {getBlockchainTransactionHash :: ByteString}
   deriving (Eq, Show, Generic)
-$(deriveJSON defaultOptions ''EventHash)
-instance ToSchema EventHash
-deriving instance FromHttpApiData EventHash
-deriving instance ToHttpApiData EventHash
+
+instance ToJSON BlockchainTransactionHash where
+  toJSON (BlockchainTransactionHash h) = toJSON . decodeUtf8 $ h
+instance FromJSON BlockchainTransactionHash where
+  parseJSON v = BlockchainTransactionHash . encodeUtf8 <$> parseJSON v
+instance ToSchema BlockchainTransactionHash where
+  declareNamedSchema _ = declareNamedSchema (Proxy :: Proxy Text)
+    <&> name ?~ "BlockchainTransactionHash"
+    <&> schema . description ?~ "Blockchain Transaction Hash"
+instance FromHttpApiData BlockchainTransactionHash where
+  parseUrlPiece t = fmap (BlockchainTransactionHash . encodeUtf8) (parseUrlPiece t)
+instance ToHttpApiData BlockchainTransactionHash where
+  toUrlPiece (BlockchainTransactionHash h) = toUrlPiece . decodeUtf8 $ h
+
+
+newtype IdentifierHash = IdentifierHash {getIdentifierHash :: ByteString}
+  deriving (Eq, Show, Generic)
+
+instance ToJSON IdentifierHash where
+  toJSON (IdentifierHash h) = toJSON . decodeUtf8 $ h
+instance FromJSON IdentifierHash where
+  parseJSON v = IdentifierHash . encodeUtf8 <$> parseJSON v
+instance ToSchema IdentifierHash where
+  declareNamedSchema _ = declareNamedSchema (Proxy :: Proxy Text)
+    <&> name ?~ "IdentifierHash"
+    <&> schema . description ?~ "Identifier Hash"
+instance FromHttpApiData IdentifierHash where
+  parseUrlPiece t = fmap (IdentifierHash . encodeUtf8) (parseUrlPiece t)
+instance ToHttpApiData IdentifierHash where
+  toUrlPiece (IdentifierHash h) = toUrlPiece . decodeUtf8 $ h
+
+
+newtype EventHash = EventHash {getEventHash :: ByteString}
+  deriving (Eq, Show, Generic)
+instance ToJSON EventHash where
+  toJSON (EventHash h) = toJSON . decodeUtf8 $ h
+instance FromJSON EventHash where
+  parseJSON v = EventHash . encodeUtf8 <$> parseJSON v
+instance ToSchema EventHash where
+  declareNamedSchema _ = declareNamedSchema (Proxy :: Proxy Text)
+    <&> name ?~ "EventHash"
+    <&> schema . description ?~ "Identifier Hash"
+instance FromHttpApiData EventHash where
+  parseUrlPiece t = fmap (EventHash . encodeUtf8) (parseUrlPiece t)
+instance ToHttpApiData EventHash where
+  toUrlPiece (EventHash h) = toUrlPiece . decodeUtf8 $ h
+
 
 data EventInsertionResponse = EventInsertionResponse
   { respEventHash :: EventHash
   , respTimeStamp :: UTCTime
   } deriving (Eq, Show, Generic)
-$(deriveJSON defaultOptions ''EventInsertionResponse)
 instance ToSchema EventInsertionResponse
+$(deriveJSON defaultOptions ''EventInsertionResponse)
