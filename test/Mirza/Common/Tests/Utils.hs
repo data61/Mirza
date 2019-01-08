9d967dfa97f65fc69a9eef3ffac1eb89d55cce8a
Ashley Stacey <ashley.stacey@data61.csiro.au>
2019-01-08 11:09:19 +1100

    [#322, #234] Revert inconsequential changes in Common/Test/Utils.hs from 6465ee7df16008fd64797b332ac6895038096958.

--- test/Mirza/Common/Tests/Utils.hs
+++ test/Mirza/Common/Tests/Utils.hs
@@ -56,22 +56,23 @@ within1Second expected actual = abs (diffUTCTime expected actual) < 1 -- second
 
 -- | Checks whether a value is between two bounds.
 -- | The comparision is done inclusive of the two bounds.
-betweenInclusive :: Ord a =>
-                       a     -- ^ One of the bounds to check
-                    -> a     -- ^ The other bound to check.
-                    -> a     -- ^ The value to check if it exists between the two bounds.
-                    -> Bool  -- ^ Whether the value is between the two bounds inclusive of the bounds.
-betweenInclusive bound1 bound2 x = (bound1 `comparitor` x) && (x `comparitor` bound2) where
-  comparitor | bound1 <= bound2  = (<=)
+betweenInclusive :: Ord a
+                 => a     -- ^ One of the bounds to check
+                 -> a     -- ^ The other bound to check.
+                 -> a     -- ^ The value to check if it exists between the two bounds.
+                 -> Bool  -- ^ Whether the value is between the two bounds inclusive of the bounds.
+betweenInclusive bound1 bound2 x = (bound1 `comparator` x) && (x `comparator` bound2) where
+  comparator | bound1 <= bound2  = (<=)
              | otherwise         = (>=)
 
+
+shouldSatisfyIO :: (HasCallStack, Show a, Eq a) => IO a -> (a -> Bool) -> Expectation
+action `shouldSatisfyIO` p = action >>= (`shouldSatisfy` p)
 
 --------------------------------------------------------------------------------
 -- Email Utils
 --------------------------------------------------------------------------------
 
-shouldSatisfyIO :: (HasCallStack, Show a, Eq a) => IO a -> (a -> Bool) -> Expectation
-action `shouldSatisfyIO` p = action >>= (`shouldSatisfy` p)
 
 -- | Only use this with hardcodes email addresses that are guaranteed to return
 -- a ``Just``
