b01f30722e80e972d8e3ca559f568b9f23804cb9
Shane Nayler <Shane.Nayler@data61.csiro.au>
2019-01-22 15:25:31 +1100

    add br bootstrapping

--- deploy/get_and_start_new_version.sh
+++ deploy/get_and_start_new_version.sh
@@ -7,11 +7,33 @@
 $(aws ecr get-login --no-include-email --region ap-southeast-2)
 
 # Stop and remove the currently running version of code
-docker-compose stop supplyChainServer businessRegistry private-ethereum-blockchain blockchain-api-server || echo "no old containers running"
-docker-compose rm -f supplyChainServer businessRegistry private-ethereum-blockchain blockchain-api-server || echo "no containers to remove"
+docker-compose stop supplyChainServer businessRegistry private-ethereum-blockchain blockchain-api-server db || echo "no old containers running"
+docker-compose rm -f supplyChainServer businessRegistry private-ethereum-blockchain blockchain-api-server db|| echo "no containers to remove"
 
 # Get the new images (defined in the docker-compose.yml) 
 docker-compose pull
+
+# Get the secrets from AWS Secrets Manager and put them in a .env file.
+
+POSTGRES_USER=$(aws --region ap-southeast-2 secretsmanager get-secret-value --secret-id development | jq -r .SecretString | sed 's/\\//g' | jq -r .POSTGRES_USER)
+POSTGRES_PASSWORD=$(aws --region ap-southeast-2 secretsmanager get-secret-value --secret-id development | jq -r .SecretString | sed 's/\\//g' | jq -r .POSTGRES_PASSWORD)
+ETH_NODE_PROTOCOL=$(aws --region ap-southeast-2 secretsmanager get-secret-value --secret-id development | jq -r .SecretString | sed 's/\\//g' | jq -r .ETH_NODE_PROTOCOL)
+ETH_NODE_HOST=$(aws --region ap-southeast-2 secretsmanager get-secret-value --secret-id development | jq -r .SecretString | sed 's/\\//g' | jq -r .ETH_NODE_HOST)
+ETH_NODE_PORT=$(aws --region ap-southeast-2 secretsmanager get-secret-value --secret-id development | jq -r .SecretString | sed 's/\\//g' | jq -r .ETH_NODE_PORT)
+ETH_ADMIN_ACCOUNT_PRIVATE_KEY=$(aws --region ap-southeast-2 secretsmanager get-secret-value --secret-id development | jq -r .SecretString | sed 's/\\//g' | jq -r .ETH_ADMIN_ACCOUNT_PRIVATE_KEY)
+BR_USER=$(aws --region ap-southeast-2 secretsmanager get-secret-value --secret-id development | jq -r .SecretString | sed 's/\\//g' | jq -r .BR_USER)
+BR_PASSWORD=$(aws --region ap-southeast-2 secretsmanager get-secret-value --secret-id development | jq -r .SecretString | sed 's/\\//g' | jq -r .BR_PASSWORD)
+
+filepath='/home/ec2-user/.env'
+
+echo "POSTGRES_USER=$POSTGRES_USER
+POSTGRES_PASSWORD=$POSTGRES_PASSWORD
+ETH_NODE_PROTOCOL=$ETH_NODE_PROTOCOL
+ETH_NODE_HOST=$ETH_NODE_HOST
+ETH_NODE_PORT=$ETH_NODE_PORT
+ETH_ADMIN_ACCOUNT_PRIVATE_KEY=$ETH_ADMIN_ACCOUNT_PRIVATE_KEY
+BR_USER=$BR_USER
+BR_PASSWORD=$BR_PASSWORD" > ${filepath}
 
 # Start the database (either an empty postgres container, or an existing database)
 docker-compose up -d db
