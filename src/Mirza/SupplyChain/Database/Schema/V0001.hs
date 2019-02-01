f8f3f8cb6e1b0392c19306016605f0f08fb5f145
Sajid Ibne Anower <sajidanower23@gmail.com>
2019-02-01 19:43:46 +1100

    Add FK constraint for users table

--- src/Mirza/SupplyChain/Database/Schema/V0001.hs
+++ src/Mirza/SupplyChain/Database/Schema/V0001.hs
@@ -105,7 +105,7 @@ migration () =
     <*> createTable "users" ( User
           lastUpdateField
           (field "user_id" pkSerialType)
-          (BizId $ field "user_biz_id" gs1CompanyPrefixType)
+          (BizId $ field "user_biz_id" gs1CompanyPrefixType (defaultFkConstraint "businesses" ["biz_gs1_company_prefix"]))
           (field "user_first_name" (varchar (Just defaultFieldMaxLength)) notNull)
           (field "user_last_name" (varchar (Just defaultFieldMaxLength)) notNull)
           (field "user_phone_number" (varchar (Just defaultFieldMaxLength)) notNull)
