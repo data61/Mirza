aba31c9916a9d0f62323b0c138f62cb3c8141b22
Paul Samways <paul.samways@data61.csiro.au>
2019-03-01 17:06:58 +1100

    Implemented event log view, up to listing events

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -3,9 +3,10 @@ import * as ReactDOM from "react-dom";
 
 import { Header } from "./components/header";
 import { Footer } from "./components/footer";
+import { EventLog } from "./components/eventLog";
 
 ReactDOM.render(<React.Fragment>
   <Header></Header>
-  <section>COntent</section>
+  <EventLog></EventLog>
   <Footer></Footer>
 </React.Fragment>, document.querySelector("main"));
