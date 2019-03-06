7e2486d0d7c040ab0577fe8e47f54c237d1b988f
Paul Samways <paul.samways@data61.csiro.au>
2019-03-06 14:42:13 +1100

    Basic integration with auth0

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -4,9 +4,21 @@ import * as ReactDOM from "react-dom";
 import { Header } from "./components/header";
 import { Footer } from "./components/footer";
 import { EventLog } from "./components/eventLog";
+import { authInit, logIn } from "./auth";
 
-ReactDOM.render(<React.Fragment>
-  <Header></Header>
-  <EventLog></EventLog>
-  <Footer></Footer>
-</React.Fragment>, document.querySelector("main"));
+authInit().then(authState => {
+  if (authState === null) {
+    logIn();
+    return;
+  }
+
+  const appState = {
+    auth: authState
+  }
+
+  ReactDOM.render(<React.Fragment>
+    <Header auth={appState.auth}></Header>
+    <EventLog></EventLog>
+    <Footer></Footer>
+  </React.Fragment>, document.querySelector("main"));
+})
