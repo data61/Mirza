2b41362478d5d694770ab1f7c40312b780c6abdd
Ashley Stacey <ashley.stacey@data61.csiro.au>
2019-03-19 14:01:39 +1100

    Remove redundant code.

--- src/Mirza/BusinessRegistry/Auth.hs
+++ src/Mirza/BusinessRegistry/Auth.hs
@@ -8,7 +8,6 @@
 module Mirza.BusinessRegistry.Auth
   (
     basicAuthServerContext
-  , authCheck
   , listUsersQuery
   ) where
 
@@ -37,51 +36,6 @@ import Servant.Auth.Server
 basicAuthServerContext :: ( Member context '[HasScryptParams, HasDB])
                        => context -> Servant.Context '[JWTSettings, CookieSettings]
 basicAuthServerContext context = defaultJWTSettings undefined :. defaultCookieSettings :. EmptyContext
-
-
--- 'BasicAuthCheck' holds the handler we'll use to verify a username and password.
--- authCheck :: SCSContext -> BasicAuthCheck ST.User
-authCheck ::  ( Member context '[HasScryptParams, HasDB])
-          => context -> BasicAuthCheck AuthUser
-authCheck context =
-  let check (BasicAuthData userEmail pass) =
-        case emailAddress userEmail of
-          Nothing -> pure Unauthorized
-          Just email -> do
-            eitherUser <- runAppM @_ @SqlError context . runDb $
-                          authCheckQuery email (Password pass)
-            case eitherUser of
-              Right (Just user) -> pure (Authorized user)
-              _                 -> pure Servant.NoSuchUser
-  in BasicAuthCheck check
-
--- Basic Auth check using Scrypt hashes.
--- TODO: How safe is this to timing attacks? Can we tell which emails are in the
--- system easily?
-authCheckQuery :: (HasScryptParams context)
-               => EmailAddress
-               -> Password
-               -> DB context err (Maybe AuthUser)
-authCheckQuery userEmail (Password password) = do
-  let userTable = _users businessRegistryDB
-  r <- pg $ runSelectReturningList $ select $ do
-        user <- all_ userTable
-        guard_ (email_address user  ==. val_ userEmail)
-        pure user
-  params <- view $ _2 . scryptParams
-  case r of
-    [user] ->
-        case Scrypt.verifyPass params (Scrypt.Pass password)
-              (Scrypt.EncryptedPass $ password_hash user)
-        of
-          (False, _     ) -> pure Nothing
-          (True, Nothing) -> pure $ Just (userToAuthUser user)
-          (True, Just (Scrypt.EncryptedPass password')) -> do
-            _ <- pg $ runUpdate $ update userTable
-                    (\u -> [password_hash u <-. val_ password'])
-                    (\u -> user_id u ==. val_ (user_id user))
-            pure $ Just (userToAuthUser user)
-    _ -> pure Nothing
 
 
 userToAuthUser :: Schema.User -> AuthUser
