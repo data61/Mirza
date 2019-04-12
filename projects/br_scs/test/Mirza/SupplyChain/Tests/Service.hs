54310e9f3f63842bc447e7ae99b1a966319fc3cd
Sajid Ibne Anower <sajidanower23@gmail.com>
2019-04-12 16:11:33 +1000

    [#415] Remove Auth module from SCS

--- projects/br_scs/test/Mirza/SupplyChain/Tests/Service.hs
+++ projects/br_scs/test/Mirza/SupplyChain/Tests/Service.hs
@@ -16,7 +16,6 @@ import           Mirza.Common.Tests.Utils                     (getDatabaseConnec
                                                                unsafeMkEmailAddress)
 import           Mirza.SupplyChain.Tests.Dummies
 
-import           Mirza.SupplyChain.Auth
 import           Mirza.SupplyChain.Database.Schema            as Schema
 import           Mirza.SupplyChain.EventUtils
 import           Mirza.SupplyChain.Handlers.Contacts
@@ -78,7 +77,7 @@ testServiceQueries = do
                 (Scrypt.EncryptedPass $ Schema.user_password_hash u)) &&
               (Schema.user_id u) == uid
             )
-    it "addUser test users with duplicate emails" $ \scsContext -> do
+    it "addUser test users with duplicate emails" $ \scsContext ->
       testAppM scsContext $  do
         _ <- addUser dummyNewUser
         catchError (addUser dummyNewUser *> pure ()) $ \err -> case err of
