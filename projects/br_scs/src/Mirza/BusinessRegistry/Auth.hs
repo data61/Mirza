403fa075d0283b326f0c2494ebe5a8117d0c0bd7
Ashley Stacey <ashley.stacey@data61.csiro.au>
2019-05-08 09:52:55 +1000

    [#385] Fix spelling mistake.
    This issue was identified in code review.

--- projects/br_scs/src/Mirza/BusinessRegistry/Auth.hs
+++ projects/br_scs/src/Mirza/BusinessRegistry/Auth.hs
@@ -12,7 +12,7 @@ module Mirza.BusinessRegistry.Auth
   , listUsersQuery
   , oauthClaimsToAuthUser
   , getUserByOAuthSubQuery
-  , userOrganisationAutherisationQuery
+  , userOrganisationAuthorisationQuery
   , checkUserExistsQuery
   ) where
 
@@ -93,12 +93,12 @@ getUserByOAuthSubQuery oauthSub = do
 -- Authorisation
 --------------------------------------------------------------------------------
 
-userOrganisationAutherisationQuery :: ( Member context '[]
+userOrganisationAuthorisationQuery :: ( Member context '[]
                                       , Member err     '[AsBRError])
                                    => AuthUser
                                    -> GS1CompanyPrefix
                                    -> DB context err OrganisationMapping
-userOrganisationAutherisationQuery (AuthUser (BT.UserId uId)) gs1CompantPrefix = do
+userOrganisationAuthorisationQuery (AuthUser (BT.UserId uId)) gs1CompantPrefix = do
   maybeMapping <- pg $ runSelectReturningOne $ select $ do
     mapping <- all_ (_organisationMapping businessRegistryDB)
     guard_ (organisation_mapping_user_id mapping ==. val_ (Schema.UserId uId))
