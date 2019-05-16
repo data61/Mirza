08b9ff6553e375a7fe33e1b7c1c1876e716cf456
Sajid Ibne Anower <sajidanower23@gmail.com>
2019-05-16 18:18:53 +1000

    Reformat

--- projects/br_scs/src/Mirza/SupplyChain/Client/Servant.hs
+++ projects/br_scs/src/Mirza/SupplyChain/Client/Servant.hs
@@ -24,18 +24,16 @@ import qualified Data.GS1.Event                     as Ev
 import           Data.GS1.EventId
 
 -- * Public API
-health       :: ClientM HealthResponse
-versionInfo  :: ClientM String
+health      :: ClientM HealthResponse
+versionInfo :: ClientM String
 
 -- * Authenticated API
-eventSign           :: SignedEvent -> ClientM EventInfo
 
-listEvents          :: LabelEPCUrn -> ClientM [Ev.Event]
-eventInfo           :: EventId -> ClientM EventInfo
-
+eventSign        :: SignedEvent -> ClientM EventInfo
+listEvents       :: LabelEPCUrn -> ClientM [Ev.Event]
+eventInfo        :: EventId -> ClientM EventInfo
 insertGS1Event   :: Ev.Event -> ClientM (EventInfo, Schema.EventId)
-
-listEventsPretty    :: LabelEPCUrn -> ClientM [PrettyEventResponse]
+listEventsPretty :: LabelEPCUrn -> ClientM [PrettyEventResponse]
 
 _api     :: Client ClientM ServerAPI
 _api@(   health
