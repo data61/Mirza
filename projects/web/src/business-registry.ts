74ff714993a14048490625b0bbb7e24f34e85fab
Paul Samways <paul.samways@data61.csiro.au>
2019-06-25 11:48:52 +1000

    Remove unneccessary commented code

--- projects/web/src/business-registry.ts
+++ projects/web/src/business-registry.ts
@@ -32,44 +32,6 @@ export class BusinessRegistry {
             companyPrefix: x.company_prefix,
             name: x.name,
         })));
-
-        //     .then(function(res: Response) {
-        //     return res.json();
-        // }).then(function(data) {
-        //     if (data[0]) {
-        //         return data[0].url;
-        //     }
-        //     return Promise.resolve();
-        // }).then(function(url) {
-        //     const [event, _] = eventState;
-        //     const dl = DigitalLink(event.epcList[0]);
-        //     if (!dl.isValid()) {
-        //         return Promise.reject("Invalid GS1 Label");
-        //     }
-        //     event.epcList[0] = dl.mapToGS1Urn();
-        //     console.log(dl.mapToGS1Urn());
-        //     console.log(JSON.stringify(event));
-        //     const request = new Request(url + '/event', {
-        //         method: 'POST',
-        //         body: JSON.stringify(event),
-        //         headers: new Headers({
-        //             'Accept': 'application/json',
-        //             'Content-Type': 'application/json',
-        //             'Authorization': token,
-        //         }),
-        //         credentials: 'include',
-        //     });
-        //     return fetch(request);
-        // }).then(function(res: Response) {
-        //     if (res.status === 200) {
-        //         alert('Success!');
-        //     } else {
-        //         alert('Failed with status: ' + res.status);
-        //     }
-        // }).catch(function(err) {
-        //     console.log(err);
-        // });
-
     }
 
     private async tryRegister(): Promise<boolean> {
