f06f011cffb4a166e8a01e5e6620d1584388ec45
Sajid Ibne Anower <sajidanower23@gmail.com>
2019-07-03 16:52:03 +1000

    Get google maps api key from env

--- projects/web/src/globals.ts
+++ projects/web/src/globals.ts
@@ -5,4 +5,5 @@ export const orUrl: string = OR_SERVICE_URL;
 export const myGlobals = {
     edapiUrl: 'http://localhost:8020' as string,
     orUrl: OR_SERVICE_URL as string,
+    googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
 };
