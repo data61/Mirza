77d8afd4f5227444c920541cbc6fb300c7651708
Ashley Stacey <ashley.stacey@data61.csiro.au>
2019-07-29 11:39:49 +1000

    [#500] Trails: Add test working notes.

--- projects/trails/test/Mirza/Trails/Tests/Client.hs
+++ projects/trails/test/Mirza/Trails/Tests/Client.hs
@@ -20,6 +20,15 @@ import           Data.Either                     (isRight)
 -- === Trails Servant Client tests
 clientSpec :: IO TestTree
 clientSpec = do
+  -- Test trail head node.
+  -- Test forked trail (1 into 2).
+  -- Test joined trail (2 into 1).
+  -- Test that invalid signature fails.
+  -- Test that invalid version fails.
+  -- Test that timestamp in the future is invalid.
+  -- Test when trying to add an entry where the parent isn't stored in the service fails.
+
+
   let healthTests = testCaseSteps "Provides health status" $ \step ->
         bracket runTrailsApp (\(a,b) -> endWaiApp (a,b)) $ \(_tid, baseurl) -> do
           let http = runClient baseurl
