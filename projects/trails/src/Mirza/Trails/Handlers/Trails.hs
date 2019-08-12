5bf00cbb9e30aafeefc6aae90e227425d318a500
Ashley Stacey <ashley.stacey@data61.csiro.au>
2019-08-12 14:51:40 +1000

    [#500] Trails: Fix code based on test failures.

--- projects/trails/src/Mirza/Trails/Handlers/Trails.hs
+++ projects/trails/src/Mirza/Trails/Handlers/Trails.hs
@@ -37,46 +37,50 @@ getTrailByEventIdQuery eventId = do
             pure entry
   case entryList of
     [] -> throwing_ _EventIdNotFoundTSE
-    entry -> concat <$> traverse getTrailBySignatureQuery (entries_signature <$> entry)
+    entry -> concat <$> traverse (getTrailBySignatureQuery []) (entries_signature <$> entry) -- TODO: Need to think more about this line, do I need to de-duplicate here...?
 
 
 getTrailBySignature :: ( Member context '[HasEnvType, HasConnPool, HasKatipContext, HasKatipLogEnv]
                        , Member err '[AsTrailsServiceError, AsSqlError])
                     => SignaturePlaceholder ->  AppM context err [TrailEntry]
 getTrailBySignature sig = do
-  runDb $ (getTrailBySignatureQuery sig)
+  runDb $ (getTrailBySignatureQuery [] sig)
 
 
+-- Algorithm is basically get a node (TrailEntry), get all of the previous and following entries, if we haven't seen
+-- them before then recurse (if we have seen them before ignore them). We need to do this in a statefull way so that we
+-- can handle loop cases (cases where the trail meets in a loop). I realised after I had a working implementation that a
+-- hash map (rather then list) was probably a better data structure to use here, but as the implementation is done and
+-- works and the expected lifetime of this code it seems wastefull to spend any more time on the implementation at present.
 getTrailBySignatureQuery :: (AsTrailsServiceError err)
-                         => SignaturePlaceholder -> DB context err [TrailEntry]
-getTrailBySignatureQuery searchSignature = do
-  previousEntries <- getPreviousEntriesBySignatureQuery searchSignature
-  followingEntries <- getPreviousEntriesQuery searchSignature
-  pure $ followingEntries <> previousEntries
+                         => [TrailEntry] -> SignaturePlaceholder -> DB context err [TrailEntry]
+getTrailBySignatureQuery discovered searchSignature = do
+  previousEntries <- getThisAndPreviousEntriesBySignatureQuery discovered searchSignature
+  getNextEntriesBySignatureQuery previousEntries searchSignature
 
 
-getPreviousEntriesQuery :: (AsTrailsServiceError err)
-                => SignaturePlaceholder -> DB context err [TrailEntry]
-getPreviousEntriesQuery searchSignature = do
+getNextEntriesBySignatureQuery :: (AsTrailsServiceError err)
+                => [TrailEntry] -> SignaturePlaceholder -> DB context err [TrailEntry]
+getNextEntriesBySignatureQuery discovered searchSignature = do
   followingSignatures <- pg $ runSelectReturningList $ select $ do
                           previous <- all_ (_previous trailsDB)
                           guard_ (previous_previous_signature previous ==. val_ searchSignature)
                           pure $ (previous_entry_signature previous)
 
-  entries <- traverse getEntryBySignature (entriesPrimaryKeyToSignature <$> followingSignatures)
-
-  followingEntries <- concat <$> traverse getPreviousEntriesQuery (trailEntrySignature <$> entries)
-
-  pure $ followingEntries <> entries
+  let newFollowingSignatures = filter (isNotPresentIn discovered) (entriesPrimaryKeyToSignature <$> followingSignatures)
+  build getTrailBySignatureQuery discovered newFollowingSignatures
 
 
-getPreviousEntriesBySignatureQuery :: (AsTrailsServiceError err)
-                           => SignaturePlaceholder -> DB context err [TrailEntry]
-getPreviousEntriesBySignatureQuery searchSignature = do
-  entry <- getEntryBySignature searchSignature
-  let previous = trailEntryParentSignatures entry
-  previousEntries <- concat <$> traverse getPreviousEntriesBySignatureQuery previous
-  pure $ entry : previousEntries
+getThisAndPreviousEntriesBySignatureQuery :: (AsTrailsServiceError err)
+                           => [TrailEntry] -> SignaturePlaceholder -> DB context err [TrailEntry]
+getThisAndPreviousEntriesBySignatureQuery discovered searchSignature = do
+  if not $ elem searchSignature (trailEntrySignature <$> discovered) then do
+    entry <- getEntryBySignature searchSignature
+    let combined = discovered <> [entry]
+    let previous = filter (isNotPresentIn combined) $ trailEntryParentSignatures entry
+    build getTrailBySignatureQuery combined previous
+  else
+    pure discovered
 
 
 getEntryBySignature :: (AsTrailsServiceError err)
@@ -94,6 +98,18 @@ getEntryBySignature searchSignature = do
                                  guard_ (previous_entry_signature previous ==. val_ (EntriesPrimaryKey searchSignature))
                                  pure previous
                     pure $ buildTrailEntry entry previous
+
+
+-- I'm sure that there is a nicer "implemenentation" of this function, something like foldl >>=, but I can't find it right now so this will do and can always refactor this later.
+build :: (Monad m) =>  ([a] -> b -> m [a]) -> [a] -> [b] -> m [a]
+build _ discovered [] = pure discovered
+build fn discovered (sig : rest) = do
+  thisEntry <- fn discovered sig
+  build fn thisEntry rest
+
+
+isNotPresentIn :: [TrailEntry] -> SignaturePlaceholder -> Bool
+isNotPresentIn discovered element = not $ elem element $ trailEntrySignature <$> discovered
 
 
 buildTrailEntry :: Entries -> [Previous] -> TrailEntry
